30abc5fb72e29a38a388244304df7b105aa9201f
Adam Morris <adam.morris@cern.ch>
2015-04-28 13:35:27 +0000

    40 files per job is too much
    git-svn-id: https://svn.cern.ch/reps/lhcbedinburgh/Code/Analysis/BsphiKK@2328 4525493e-7705-40b1-a816-d608a930855b

--- gridjobs/Ganga-DaVinci.py
+++ gridjobs/Ganga-DaVinci.py
@@ -13,7 +13,7 @@ myApplication.user_release_area = '/afs/cern.ch/user/a/admorris/cmtuser'
 myApplication.platform='x86_64-slc6-gcc48-opt'
 #optFile =  [ './DaVinci-Opts.py', './myUp.py']
 
-mySplitter = SplitByFiles(filesPerJob = 40, maxFiles = -1, ignoremissing = True, bulksubmit=True)
+mySplitter = SplitByFiles(filesPerJob = 20, maxFiles = -1, ignoremissing = True, bulksubmit=True)
 myOutput = [ DiracFile('BsphiKK.root'), DiracFile('dummy.root'), LocalFile('summary.xml')]
 myBackend = Dirac()
 dirc = '~/work/BsphiKK/gridjobs/'
