818bbf46616ffb76f370d9ff7b785d7067f893b2
Adam Morris <adam.morris@cern.ch>
2015-10-05 11:48:48 +0200

    More scripts

---
+++ scripts/ANALYSIS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A script to keep track of all the other scripts. Assumes the DaVinci output is available on the grid. If not, see the folder gridjobs.
+# Please run all scripts from within the scripts folder
+#./getoutputdata
+#./merge.sh
+#./cuts.sh
+./addBranches.sh
+./vetoes.sh
+./sideband.sh
+./trainmva.sh
+./applymva.sh
+./mvacut.sh
---
+++ scripts/trainmva.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd ../mva
+root -l -q -b ZTMVAClassification.C+
