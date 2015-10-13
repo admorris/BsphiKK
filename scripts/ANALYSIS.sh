e8e65741b457efa0df8ea74ff2dda331dce13ead
Adam Morris <adam.morris@cern.ch>
2015-10-13 13:42:43 +0200

    script output writes to files

--- scripts/ANALYSIS.sh
+++ scripts/ANALYSIS.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # A script to keep track of all the other scripts. Assumes the DaVinci output is available on the grid. If not, see the folder gridjobs.
 # Please run all scripts from within the scripts folder
-#./getoutputdata
-#./merge.sh
-./cut.sh
-./addBranches.sh
-./vetoes.sh
-./sweight.sh
-./trainmva.sh
-./applymva.sh
-./mvacut.sh
+#./getoutputdata > log/getoutputdata.log
+#./merge.sh > log/merge.log
+./cut.sh > log/cut.log
+./addBranches.sh > log/addBranches.log
+./vetoes.sh > log/vetoes.log
+./sweight.sh > log/sweight.log
+./trainmva.sh > log/trainmva.log
+./applymva.sh > log/applymva.log
+./mvacut.sh > log/mvacut.log
