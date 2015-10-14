bbfd5baaff071a0611f55acb4436f66e426c8e49
Adam Morris <adam.morris@cern.ch>
2015-10-14 15:07:34 +0200

    formatting

--- src/app/PlotBsMasses.cpp
+++ src/app/PlotBsMasses.cpp
@@ -22,12 +22,12 @@ TH1D* plotBsmass(string filename = "LbphiKp_MC")
   // Get the input tree
   TTree* intree  = (TTree*)infile->Get("DecayTree");
   // Set branches to read
-  Double_t B_s0_LOKI_Mass;
+  double B_s0_LOKI_Mass;
   intree->SetBranchAddress("B_s0_LOKI_Mass",&B_s0_LOKI_Mass);
 /*Output***********************************************************************/
   TH1D* hist = new TH1D(filename.c_str(),"",100,5170,5580);
 /*Fill*************************************************************************/
-  for(Int_t i = 0; i < intree->GetEntries(); i++)
+  for(int i = 0; i < intree->GetEntries(); i++)
   {
     intree->GetEntry(i);
     hist->Fill(B_s0_LOKI_Mass);
@@ -38,37 +38,37 @@ void plotBsmasses()
 {
 /*Input variables**************************************************************/
   // Generated events
-  Int_t ngen_LbphiKp   =  511582+ 540787;
-  Int_t ngen_BdphiKst  = 2007492+2020493;
-  Int_t ngen_Bsphipipi = 4004542+9016672;
-  Int_t ngen_Bsphiphi  = 1028748+1029997;
-  Int_t ngen_BsphiKK   = ngen_Bsphiphi;
+  int ngen_LbphiKp   =  511582+ 540787;
+  int ngen_BdphiKst  = 2007492+2020493;
+  int ngen_Bsphipipi = 4004542+9016672;
+  int ngen_Bsphiphi  = 1028748+1029997;
+  int ngen_BsphiKK   = ngen_Bsphiphi;
   // Fragmentation fraction ratios
-  Double_t fLb_fufd = 0.404*(1-0.031*5); // 0.404(1-0.031*pT(GeV))
-  Double_t fs_fufd  = 0.134;
-  Double_t fs_fd    = 0.267;
+  double fLb_fufd    = 0.404*(1-0.031*5); // 0.404(1-0.031*pT(GeV))
+  double fs_fufd     = 0.134;
+  double fs_fd       = 0.267;
   // Branching fractions
-  Double_t B_phitoKK   =   0.489;
-  Double_t B_KsttoKpi  = 2.0/3.0;
-  Double_t B_Bsphiphi  = 1.82e-5; // Get B_BsphiKK from B_BsJpsiKK, B_BsJpsiphi and B_Bsphiphi
-  Double_t B_BsJpsiphi = 1.05e-3;
-  Double_t B_BsJpsiKK  = 7.70e-4-B_phitoKK*B_BsJpsiphi; // Assumes
-  Double_t B_BsphiKK   = B_BsJpsiKK*B_Bsphiphi/B_BsJpsiphi;
-  Double_t B_LbJpsipK  = 3.04e-4; // LHCb-PAPER-2015-032 draft
-  Double_t B_LbphiKp   = B_LbJpsipK*B_Bsphiphi/B_BsJpsiphi;
-  Double_t B_Bsphipipi = 3.76e-6; // Haofei's result
-  Double_t B_BdphiKst  = 1.00e-5;
+  double B_phitoKK   =   0.489;
+  double B_KsttoKpi  = 2.0/3.0;
+  double B_Bsphiphi  = 1.82e-5; // Get B_BsphiKK from B_BsJpsiKK, B_BsJpsiphi and B_Bsphiphi
+  double B_BsJpsiphi = 1.05e-3;
+  double B_BsJpsiKK  = 7.70e-4-B_phitoKK*B_BsJpsiphi; // Assumes
+  double B_BsphiKK   = B_BsJpsiKK*B_Bsphiphi/B_BsJpsiphi;
+  double B_LbJpsipK  = 3.04e-4; // LHCb-PAPER-2015-032 draft
+  double B_LbphiKp   = B_LbJpsipK*B_Bsphiphi/B_BsJpsiphi;
+  double B_Bsphipipi = 3.76e-6; // Haofei's result
+  double B_BdphiKst  = 1.00e-5;
 /*Get the histograms***********************************************************/
-  TH1D* LbphiKp   = plotBsmass("LbphiKp_MC"  );
-  TH1D* BdphiKst  = plotBsmass("BdphiKst_MC" );
-  TH1D* Bsphipipi = plotBsmass("Bsphipipi_MC");
-  TH1D* BsphiKK   = plotBsmass("BsphiKK_MC"  );
-  TCanvas* can    = new TCanvas("can","");
+  TH1D* LbphiKp      = plotBsmass("LbphiKp_MC"  );
+  TH1D* BdphiKst     = plotBsmass("BdphiKst_MC" );
+  TH1D* Bsphipipi    = plotBsmass("Bsphipipi_MC");
+  TH1D* BsphiKK      = plotBsmass("BsphiKK_MC"  );
+  TCanvas* can       = new TCanvas("can","");
 /*Calculate normalisation for each mode****************************************/
-  Double_t denominator = B_BsphiKK  *(1.0/ngen_BsphiKK);
-  Double_t w_LbphiKp   = B_LbphiKp  *(1.0/ngen_LbphiKp)  /denominator;
-  Double_t w_BdphiKst  = B_BdphiKst *(1.0/ngen_BdphiKst) *B_KsttoKpi/denominator;
-  Double_t w_Bsphipipi = B_Bsphipipi*(1.0/ngen_Bsphipipi)/denominator;
+  double denominator = B_BsphiKK  *(1.0/ngen_BsphiKK);
+  double w_LbphiKp   = B_LbphiKp  *(1.0/ngen_LbphiKp)  /denominator;
+  double w_BdphiKst  = B_BdphiKst *(1.0/ngen_BdphiKst) *B_KsttoKpi/denominator;
+  double w_Bsphipipi = B_Bsphipipi*(1.0/ngen_Bsphipipi)/denominator;
   BsphiKK->Scale(1.0); // Probably don't need this line
   cout << "N_LbphiKp   = \t" << w_LbphiKp*(fLb_fufd/fs_fufd)*LbphiKp->GetEntries()  /BsphiKK->GetEntries() << "×N_BsphiKK" << endl
        << "N_BdphiKst  = \t" << w_BdphiKst/fs_fd            *BdphiKst->GetEntries() /BsphiKK->GetEntries() << "×N_BsphiKK" << endl
