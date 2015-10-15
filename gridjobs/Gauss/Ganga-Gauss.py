376ab819cf6b54109b6930fbbb7d404fd098884a
Adam Morris <adam.morris@cern.ch>
2015-10-15 15:11:20 +0200

    stuff to run gauss

---
+++ gridjobs/Gauss/Ganga-Gauss.py
@@ -0,0 +1,28 @@
+# Tested with Ganga 6.1.11
+
+import sys
+import os
+
+GaussVersion = "v48r0"
+
+GaussDir = "/afs/cern.ch/lhcb/software/releases/GAUSS/GAUSS_"+GaussVersion
+OptnsDir = GaussDir+"/Sim/Gauss/options"
+PythiDir = GaussDir+"/Gen/LbPythia"
+CurntDir = os.getcwd()
+
+if len(sys.argv) is not 2:
+  sys.exit("Please provide an event type file.")
+
+j = Job(application=Gauss(version=GaussVersion))
+j.application.optsfile = [
+    OptnsDir+"/Gauss-2012.py"
+  , CurntDir+"/"+sys.argv[1]
+  , PythiDir+"/options/Pythia.py"
+  , OptnsDir+"/GenStandAlone.py"
+  , OptnsDir+"/Gauss-Job.py"
+  ]
+j.name = "Default Generation"
+j.splitter = GaussSplitter(numberOfJobs=10,eventsPerJob=10)
+j.outputfiles = [DiracFile("*.xgen"),DiracFile("*.root"),DiracFile("*.xml")]
+j.backend = Dirac()
+j.submit()
