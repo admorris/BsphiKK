6bc75b6267215b6d44b6b4382478fb9749ee6d26
Adam Morris <adam.morris@cern.ch>
2015-10-15 15:10:58 +0200

    ad hoc stuff not necessary

--- gridjobs/Gauss/13104024.py
+++ gridjobs/Gauss/13104024.py
@@ -19,27 +19,27 @@ Generation().SignalRepeatedHadronization.SignalPIDList = [ 531,-531 ]
 
 # Ad-hoc particle gun code
 
-from Configurables import ParticleGun
-pgun = ParticleGun("ParticleGun")
-pgun.SignalPdgCode = 531
-pgun.DecayTool = "EvtGenDecay"
-pgun.GenCutTool = "DaughtersInLHCbAndWithDaughAndBCuts"
+#from Configurables import ParticleGun
+#pgun = ParticleGun("ParticleGun")
+#pgun.SignalPdgCode = 531
+#pgun.DecayTool = "EvtGenDecay"
+#pgun.GenCutTool = "DaughtersInLHCbAndWithDaughAndBCuts"
 
-pgun.addTool( Generation().SignalRepeatedHadronization.DaughtersInLHCbAndWithDaughAndBCuts.clone() )
+#pgun.addTool( Generation().SignalRepeatedHadronization.DaughtersInLHCbAndWithDaughAndBCuts.clone() )
 
-from Configurables import FlatNParticles
-pgun.NumberOfParticlesTool = "FlatNParticles"
-pgun.addTool( FlatNParticles , name = "FlatNParticles" )
+#from Configurables import FlatNParticles
+#pgun.NumberOfParticlesTool = "FlatNParticles"
+#pgun.addTool( FlatNParticles , name = "FlatNParticles" )
 
-from Configurables import MomentumSpectrum
-pgun.ParticleGunTool = "MomentumSpectrum"
-pgun.addTool( MomentumSpectrum , name = "MomentumSpectrum" )
-pgun.MomentumSpectrum.PdgCodes = [ 531,-531 ]
-pgun.MomentumSpectrum.InputFile = "$PGUNSDATAROOT/data/Ebeam4000GeV/MomentumSpectrum_531.root"
-pgun.MomentumSpectrum.BinningVariables = "pteta"
-pgun.MomentumSpectrum.HistogramPath = "h_pteta"
+#from Configurables import MomentumSpectrum
+#pgun.ParticleGunTool = "MomentumSpectrum"
+#pgun.addTool( MomentumSpectrum , name = "MomentumSpectrum" )
+#pgun.MomentumSpectrum.PdgCodes = [ 531,-531 ]
+#pgun.MomentumSpectrum.InputFile = "$PGUNSDATAROOT/data/Ebeam4000GeV/MomentumSpectrum_531.root"
+#pgun.MomentumSpectrum.BinningVariables = "pteta"
+#pgun.MomentumSpectrum.HistogramPath = "h_pteta"
 
-from Configurables import BeamSpotSmearVertex
-pgun.addTool(BeamSpotSmearVertex, name="BeamSpotSmearVertex")
-pgun.VertexSmearingTool = "BeamSpotSmearVertex"
-pgun.EventType = 13104024
+#from Configurables import BeamSpotSmearVertex
+#pgun.addTool(BeamSpotSmearVertex, name="BeamSpotSmearVertex")
+#pgun.VertexSmearingTool = "BeamSpotSmearVertex"
+#pgun.EventType = 13104024
