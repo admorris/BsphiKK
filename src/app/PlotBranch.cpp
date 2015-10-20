e5908608deab1b09d2067d7a339b787637ba2257
Adam Morris <adam.morris@cern.ch>
2015-10-20 18:13:01 +0200

    option to weight distribution

--- src/app/PlotBranch.cpp
+++ src/app/PlotBranch.cpp
@@ -12,14 +12,24 @@
 // Custom headers
 #include "plotmaker.h"
 
-void PlotBranch(string filename, string branchname, string xtitle, string unit, string plotname, string cuts, double xlow, double xup, int nbins)
+void PlotBranch(string filename, string branchname, string xtitle, string unit, string plotname, string cuts, string weight, double xlow, double xup, int nbins)
 {
   TFile* file = new TFile(filename.c_str());
   TTree* tree = (TTree*)file->Get("DecayTree");
   using namespace RooFit;
   RooRealVar* x = new RooRealVar(branchname.c_str(),xtitle.c_str(),xlow,xup);
+  RooRealVar* w;
+  RooDataSet* data;
   std::cout << "Importing tree" << endl;
-  RooDataSet* data = new RooDataSet("data","",RooArgSet(*x),Import(*tree),cuts.c_str());
+  if(weight!="")
+  {
+    w = new RooRealVar(weight.c_str(),"",-0.5,1.5);
+    data = new RooDataSet("data","",RooArgSet(*x,*w),WeightVar(*w),Import(*tree));
+  }
+  else
+  {
+    data = new RooDataSet("data","",RooArgSet(*x),Import(*tree));
+  }
   RooPlot* frame = x->frame();
   std::cout << "Plotting" << endl;
   data->plotOn(frame,Binning(nbins));
@@ -32,13 +42,14 @@ int main(int argc, char* argv[])
 {
   using namespace boost::program_options;
   options_description desc("Allowed options");
-  std::string file, branch, cuts, xtitle, unit, plot;
+  std::string file, branch, cuts, xtitle, unit, plot, weight;
   double xlow, xup;
   int nbins;
   desc.add_options()
     ("help,H"  ,                                                                                                    "produce help message"                      )
     ("file,F"  , value<std::string>(&file  )->default_value("ntuples/BsphiKK_data_mva.root"                      ), "set data file"                             )
     ("branch,B", value<std::string>(&branch)->default_value("B_s0_LOKI_Mass"                                     ), "set branch to plot"                        )
+    ("weight,W", value<std::string>(&weight)->default_value(""                                                   ), "set weighting variable"                    )
     ("cuts,C"  , value<std::string>(&cuts  )->default_value(""                                                   ), "set optional cuts"                         )
     ("title,T" , value<std::string>(&xtitle)->default_value("#it{m}(#it{K^{#plus}K^{#minus}K^{#plus}K^{#minus}})"), "set x-axis title (takes ROOT LaTeX format)")
     ("unit,U"  , value<std::string>(&unit  )->default_value("MeV/#it{c}^{2}"                                     ), "set unit (takes ROOT LaTeX format)"        )
@@ -46,6 +57,7 @@ int main(int argc, char* argv[])
     ("upper,u" , value<double     >(&xup   )->default_value(5600                                                 ), "set branch upper limit"                    )
     ("lower,l" , value<double     >(&xlow  )->default_value(5200                                                 ), "set branch lower limit"                    )
     ("bins,b"  , value<int        >(&nbins )->default_value(50                                                   ), "set number of bins"                        )
+    
   ;
   variables_map vmap;
   store(parse_command_line(argc, argv, desc), vmap);
@@ -56,6 +68,6 @@ int main(int argc, char* argv[])
     return 1;
   }
   cout << "Entering main function" << endl;
-  PlotBranch(file,branch,xtitle,unit,plot,cuts,xlow,xup,nbins);
+  PlotBranch(file,branch,xtitle,unit,plot,cuts,weight,xlow,xup,nbins);
   return 0;
 }
