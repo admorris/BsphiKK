0cc97120906610afdc0fdf3ed9c01ba754f0b06c
Adam Morris <adam.morris@cern.ch>
2015-10-22 16:53:18 +0200

    version assuming everything is on EOS as it should be

--- scripts/ANALYSIS.sh
+++ scripts/ANALYSIS.sh
@@ -3,11 +3,11 @@
 # Please run all scripts from within the scripts folder
 #./getoutputdata | tee log/getoutputdata.log
 #./merge.sh | tee log/merge.log
-#./docuts.sh | tee log/docuts.log
-#./addBranches.sh | tee log/addBranches.log
-#./dovetoes.sh | tee log/dovetoes.log
-#./seleff.sh
-#./trigeff.sh
+./docuts.sh | tee log/docuts.log
+./addBranches.sh | tee log/addBranches.log
+./dovetoes.sh | tee log/dovetoes.log
+./seleff.sh
+./trigeff.sh
 ./sweight.sh | tee log/sweight.log
 ./mvadataMCsWcomp.sh | tee log/mvadataMCsWcomp.log
 ./trainmva.sh | tee log/trainmva.log
