e7e16bac6ea8eefe8d444363bd7c09545175d6b3
Adam Morris <adam.morris@cern.ch>
2015-11-05 16:07:31 +0100

    optimal cut

--- scripts/mvacut.sh
+++ scripts/mvacut.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 for mode in $(ls ../ntuples/*_mva.root | sed 's/_mva\.root//'); do
-cutapplier ${mode}_mva.root DecayTree "mlp>0.7159" ${mode}_mvacut.root
+cutapplier ${mode}_mva.root DecayTree "mlp>0.62" ${mode}_mvacut.root
 done
 exit 0
