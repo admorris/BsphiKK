4a1853d77996857228a6481a6e541625e5bd445f
Adam Morris <adam.morris@cern.ch>
2015-11-24 18:38:34 +0100

    fixes so compiles. added M(m)

--- fits/pdfs/src/Bs2PhiKKComponent.cpp
+++ fits/pdfs/src/Bs2PhiKKComponent.cpp
@@ -3,7 +3,14 @@
 #include "DPBWResonanceShape.hh"
 #include "DPFlatteShape.hh"
 #include "Bs2PhiKKComponent.h"
-
+#include "DPBarrierFactor.hh"
+#include "DPBarrierL0.hh"
+#include "DPBarrierL1.hh"
+#include "DPBarrierL2.hh"
+#include "SphericalHarmonic.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 Bs2PhiKKComponent::Bs2PhiKKComponent(int J2, double M2, double W2, string shape) : 
     _J1(1)        // Spin of phi --> fixed
   , _J2(J2)       // Spin of particle 2
@@ -12,14 +19,14 @@ Bs2PhiKKComponent::Bs2PhiKKComponent(int J2, double M2, double W2, string shape)
   , _W1(4.266)    // Width of phi --> fixed
   , _W2(W2)       // Width of particle 2
 {
-  _lambda_max = TMath::Min(J1,J2);
+  _lambda_max = TMath::Min(_J1,_J2);
   for(int lambda = -_lambda_max; lambda <= +_lambda_max; lambda++)
   {
     _A.push_back(TComplex(sqrt(1.0/(1+2*_lambda_max)),0)); // Make our helicity amplitudes
   }
   if(shape=="BW")
   {
-    _M = new DPBWResonanceShape(M2,W2,L,493.677,493.677,R); // Find out what L and R are
+    _M = new DPBWResonanceShape(M2,W2,J2,493.677,493.677,1.7); // Typical values of barrier factor radius are 3 and 1.7 inverse GeV
   }
   if(shape=="FT")
   {
@@ -30,27 +37,31 @@ TComplex* Bs2PhiKKComponent::A(int lambda)
 {
   return &_A[lambda+_lambda_max]; // Return the corresponding helicity amplitude given a value of helicity
 }
-TComplex Bs2PhiKKComponent::F(double phi, double theta1, double theta2)
+TComplex Bs2PhiKKComponent::F(double Phi, double theta1, double theta2)
 {
   TComplex result(0, 0);
   for(int lambda = -_lambda_max; lambda <= +_lambda_max; lambda++)
   {
-    result += *A(lambda) * Y(-lambda, _J1, TMath::Pi - theta_1, -Phi)*Y(lambda, _J2, theta2, 0);
+    result += *A(lambda) * Y(-lambda, _J1, TMath::Pi() - theta1, -Phi)*Y(lambda, _J2, theta2, 0);
   }
   return result;
 }
 TComplex Bs2PhiKKComponent::Amplitude(double m, double phi, double cos_theta1, double cos_theta2)
 {
-  return M(m)*F(phi, TMath::Acos(cos_theta1), TMath::Acos(cos_theta2));
+  return M(m)*F(phi, TMath::ACos(cos_theta1), TMath::ACos(cos_theta2));
 }
 void Bs2PhiKKComponent::SetParameters(vector<TComplex> newA)
 {
   if(newA.size()!=_A.size())
   {
-    cout << newA.size() << " given, but " << _A.size() << " required!" << endl;
+    cout << newA.size() << " amplitudes given, but " << _A.size() << " required!" << endl;
   }
   for(unsigned int i = 0; i < _A.size(); i++)
   {
     _A[i] = newA[i];
   }
+}
+TComplex Bs2PhiKKComponent::M(double m)
+{
+  return _M->massShape(m);
 }
