c0b21869374b76b985c029f7de18b420f214bded
Adam Morris <adam.morris@cern.ch>
2015-11-24 18:39:06 +0100

    completely re-parametersised the helicity amplitudes

--- fits/pdfs/src/Bs2PhiKKTotal.cpp
+++ fits/pdfs/src/Bs2PhiKKTotal.cpp
@@ -19,29 +19,11 @@ PDF_CREATOR( Bs2PhiKKTotal )
 
 //Constructor
 Bs2PhiKKTotal::Bs2PhiKKTotal(PDFConfigurator* config) :
-  // Physics parameters
-    ASzero2(0.0)
-  , APzero2(0.0)
-  , APpara2(0.0)
-  , ADzero2(0.0)
-  , ADpara2(0.0)
-  , nSwave(0.0)
-  , nPwave(0.0)
-  , nDwave(0.0)
   // Observables
-  , mKK(0.0)
+    mKK(0.0)
   , phi(0.0)
   , ctheta_1(0.0)
   , ctheta_2(0.0)
-  // Physics parameters
-  , ASzero2Name   ( config->getName("ASzero2" ) )
-  , APzero2Name   ( config->getName("APzero2" ) )
-  , APpara2Name   ( config->getName("APpara2" ) )
-  , ADzero2Name   ( config->getName("ADzero2" ) )
-  , ADpara2Name   ( config->getName("ADpara2" ) )
-  , nSwaveName    ( config->getName("nSwave"  ) )
-  , nPwaveName    ( config->getName("nPwave"  ) )
-  , nDwaveName    ( config->getName("nDwave"  ) )
   // Observables
   , mKKName       ( config->getName("mKK"     ) )
   , ctheta_1Name  ( config->getName("ctheta_1") )
@@ -50,14 +32,40 @@ Bs2PhiKKTotal::Bs2PhiKKTotal(PDFConfigurator* config) :
   // Options
   , init(true)
 {
-    MakePrototypes();
-    cout << "Making PhiKK" << endl;
-    Bs2PhiKKComponent* Swave = new Bs2PhiKKComponent(0, 980    ,100    ,"FT");
-    Bs2PhiKKComponent* Pwave = new Bs2PhiKKComponent(1,1019.461,  4.266,"BW");
-    Bs2PhiKKComponent* Dwave = new Bs2PhiKKComponent(2,1525    , 73    ,"BW");
+  // Physics parameters
+  ASsq      = 0;
+  deltaS    = 0;
+  for(unsigned short i = 0; i < 3; i++)
+  {
+    APsq[i]   = 0;
+    ADsq[i]   = 0;
+    deltaP[i] = 0;
+    deltaD[i] = 0;
+  }
+  // Magnitude-squared of helicity amplitudes
+  ASsqName    = config->getName("ASzero2" );
+  APsqName[0] = config->getName("APminus2");
+  APsqName[1] = config->getName("APzero2" );
+  APsqName[2] = config->getName("APplus2" );
+  ADsqName[0] = config->getName("ADminus2");
+  ADsqName[1] = config->getName("ADzero2" );
+  ADsqName[2] = config->getName("ADplus2" );
+  // Phases of helicity amplitudes
+  deltaSName    = config->getName("deltaSzero" );
+  deltaPName[0] = config->getName("deltaPminus");
+  deltaPName[1] = config->getName("deltaPzero" );
+  deltaPName[2] = config->getName("deltaPplus" );
+  deltaDName[0] = config->getName("deltaDminus");
+  deltaDName[1] = config->getName("deltaDzero" );
+  deltaDName[2] = config->getName("deltaDplus" );  
+  MakePrototypes();
+  cout << "Making PhiKK" << endl;
+  Swave = new Bs2PhiKKComponent(0, 980    ,100    ,"FT");
+  Pwave = new Bs2PhiKKComponent(1,1019.461,  4.266,"BW");
+  Dwave = new Bs2PhiKKComponent(2,1525    , 73    ,"BW");
 }
 //Destructor
-Bs2PhiKKComponent::~Bs2PhiKKComponent()
+Bs2PhiKKTotal::~Bs2PhiKKTotal()
 {
   delete Swave;
   delete Pwave;
@@ -73,14 +81,15 @@ void Bs2PhiKKTotal::MakePrototypes()
   allObservables.push_back( phiName      );
   //Make the parameter set
   vector<string> parameterNames;
-  parameterNames.push_back( ASzero2Name );
-  parameterNames.push_back( APzero2Name );
-  parameterNames.push_back( APpara2Name );
-  parameterNames.push_back( ADzero2Name );
-  parameterNames.push_back( ADpara2Name );
-  parameterNames.push_back( nSwaveName  );
-  parameterNames.push_back( nPwaveName  );
-  parameterNames.push_back( nDwaveName  );
+  parameterNames.push_back( ASsqName );
+  parameterNames.push_back( deltaSName );
+  for(unsigned short i = 0; i < 3; i++)
+  {
+    parameterNames.push_back( APsqName[i] );
+    parameterNames.push_back( ADsqName[i] );
+    parameterNames.push_back( deltaPName[i] );
+    parameterNames.push_back( deltaDName[i] );
+  }
   allParameters = *( new ParameterSet(parameterNames) );
   cout << "Finished making prototypes" << endl;
 }
@@ -88,16 +97,16 @@ void Bs2PhiKKTotal::MakePrototypes()
 //Not only set the physics parameters, but indicate that the cache is no longer valid
 bool Bs2PhiKKTotal::SetPhysicsParameters( ParameterSet * NewParameterSet )
 {
-  bool isOK     = allParameters.SetPhysicsParameters(NewParameterSet);
-  ASzero2       = allParameters.GetPhysicsParameter( ASzero2Name    )->GetValue();
-  APzero2       = allParameters.GetPhysicsParameter( APzero2Name    )->GetValue();
-  APpara2       = allParameters.GetPhysicsParameter( APpara2Name    )->GetValue();
-  ADzero2       = allParameters.GetPhysicsParameter( ADzero2Name    )->GetValue();
-  ADpara2       = allParameters.GetPhysicsParameter( ADpara2Name    )->GetValue();
-  nSwave        = allParameters.GetPhysicsParameter( nSwaveName     )->GetValue();
-  nPwave        = allParameters.GetPhysicsParameter( nPwaveName     )->GetValue();
-  nDwave        = allParameters.GetPhysicsParameter( nDwaveName     )->GetValue();
-  
+  bool isOK = allParameters.SetPhysicsParameters(NewParameterSet);
+  ASsq      = allParameters.GetPhysicsParameter(ASsqName     )->GetValue();
+  deltaS    = allParameters.GetPhysicsParameter(deltaSName   )->GetValue();
+  for(unsigned short i = 0; i < 3; i++)
+  {
+    APsq[i]   = allParameters.GetPhysicsParameter(APsqName[i]  )->GetValue();
+    ADsq[i]   = allParameters.GetPhysicsParameter(ADsqName[i]  )->GetValue();
+    deltaP[i] = allParameters.GetPhysicsParameter(deltaPName[i])->GetValue();
+    deltaD[i] = allParameters.GetPhysicsParameter(deltaDName[i])->GetValue();
+  }
   return isOK;
 }
 //Return a list of parameters not to be integrated
@@ -109,39 +118,39 @@ vector<string> Bs2PhiKKTotal::GetDoNotIntegrateList()
 //Calculate the function value
 double Bs2PhiKKTotal::Evaluate(DataPoint * measurement)
 {
-  mKK      = measurement->GetObservable(mKKname     )->GetValue();
+  mKK      = measurement->GetObservable(mKKName     )->GetValue();
   ctheta_1 = measurement->GetObservable(ctheta_1Name)->GetValue();
   ctheta_2 = measurement->GetObservable(ctheta_2Name)->GetValue();
   phi      = measurement->GetObservable(phiName     )->GetValue();
   // S-wave
   vector<TComplex> AS;
-  AS.push_back(TComplex(sqrt(ASzero2), deltaS, true);
-  Swave.SetParameters(AS);
-  // P-wave
-  TComplex APperp(sqrt(APperp2()), deltaPperp, true);
-  TComplex APpara(sqrt(APpara2), deltaPpara, true);
-  vector<TComplex> AP;
-  AP.push_back((APpara-APperp)/sqrt(2));
-  AP.push_back(TComplex(sqrt(APzero2), deltaPzero, true);
-  AP.push_back((APpara+APperp)/sqrt(2));
-  Swave.SetParameters(AP);
-  // D-wave
-  TComplex ADperp(sqrt(ADperp2()), deltaDperp, true);
-  TComplex ADpara(sqrt(ADpara2), deltaDpara, true);
-  vector<TComplex> AD;
-  AD.push_back((ADpara-ADperp)/sqrt(2));
-  AD.push_back(TComplex(sqrt(ADzero2), deltaDzero, true);
-  AD.push_back((ADpara+ADperp)/sqrt(2));
-  Swave.SetParameters(AD);
+  AS.push_back(TComplex(sqrt(ASsq), deltaS, true));
+  Swave->SetParameters(AS);
+  // P- and D-wave
+  vector<TComplex> AP, AD;
+  for(unsigned short i = 0; i < 3; i++)
+  {
+    AP.push_back(TComplex(sqrt(APsq[i]), deltaP[i], true));
+    AD.push_back(TComplex(sqrt(ADsq[i]), deltaD[i], true));
+  }
+  Pwave->SetParameters(AP);
+  Dwave->SetParameters(AD);
   
-  double evalres
-  TComplex amplitude = sqrt(nSwave) * Swave.Amplitude(mKK, phi, ctheta_1, ctheta_2)
-                     + sqrt(nPwave) * Pwave.Amplitude(mKK, phi, ctheta_1, ctheta_2)
-                     + sqrt(nDwave) * Dwave.Amplitude(mKK, phi, ctheta_1, ctheta_2);
+  double evalres;
+  TComplex amplitude = Swave->Amplitude(mKK, phi, ctheta_1, ctheta_2)
+                     + Pwave->Amplitude(mKK, phi, ctheta_1, ctheta_2)
+                     + Dwave->Amplitude(mKK, phi, ctheta_1, ctheta_2);
   evalres = amplitude.Rho2();
   return evalres;
 }
 double Bs2PhiKKTotal::Normalisation(DataPoint * measurement, PhaseSpaceBoundary * boundary)
 {
- 	return nSwave + nPwave + nDwave;
+  double norm = 0;
+  norm += ASsq;
+  for(unsigned short i = 0; i < 3; i++)
+  {
+    norm+=APsq[i];
+    norm+=ADsq[i];
+  }
+ 	return norm;
 }
