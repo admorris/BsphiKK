bc8bb348d59509ac2e04b49c3ef7ce0f5d48d493
Adam Morris <adam.morris@cern.ch>
2015-12-11 09:54:39 +0100

    methods

--- fits/pdfs/include/Bs2PhiKKAcceptance.h
+++ fits/pdfs/include/Bs2PhiKKAcceptance.h
@@ -1,9 +1,17 @@
 class Bs2PhiKKAcceptance
 {
   public:
-    Bs2PhiKKAcceptance();
-//    Bs2PhiKKAcceptance(Bs2PhiKKAcceptance&);
-//    ~Bs2PhiKKAcceptance();
-  private:
+    Bs2PhiKKAcceptance(double,double);
+    Bs2PhiKKAcceptance(const Bs2PhiKKAcceptance&);
+    ~Bs2PhiKKAcceptance();
+    double Evaluate(double,double,double,double);
+  protected:
     double**** c;
+    void createcoefficients();
+    double mKK_min;
+    double mKK_max;
+    static int l_max;
+    static int i_max;
+    static int k_max;
+    static int j_max;
 };
