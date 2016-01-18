efe6b405669d01b0efc42d00c2fee6bfaf6b8c39
Adam Morris <adam.morris@cern.ch>
2016-01-18 13:11:25 +0100

    typos

--- scripts/doBsmassfit.sh
+++ scripts/doBsmassfit.sh
@@ -9,13 +9,13 @@ cd ../ntuples/
 ../bin/BsMassFit \
     -M ../ntuples/BsphiKK_MC_1050_mvacut.root \
     -R ../ntuples/BsphiKK_data_1050_mvacut.root \
-    -O ../latex/figs/Bsmassfit_1050_mvacut 
+    -O ../latex/figs/Bsmassfit_1050_mvacut \
     --pulls
 
 ../bin/BsMassFit \
     -M ../ntuples/BsphiKK_MC_1800_mvacut.root \
     -R ../ntuples/BsphiKK_data_1800_mvacut.root \
-    -O ../latex/figs/Bsmassfit_1800_mvacut 
+    -O ../latex/figs/Bsmassfit_1800_mvacut \
     --pulls
 
 ../bin/BsMassFit \
