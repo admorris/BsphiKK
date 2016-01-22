1ded3ec48f204573a01d7d0d978c05ca5163453a
Adam Morris <adam.morris@cern.ch>
2016-01-22 12:23:56 +0100

    more general

--- src/app/GetResolution.cpp
+++ src/app/GetResolution.cpp
@@ -1,5 +1,7 @@
 // Standard headers
 #include <string>
+#include <iostream>
+#include <vector>
 // BOOST headers
 #include "boost/program_options.hpp"
 // ROOT headers
@@ -11,11 +13,15 @@
 #include "RooPlot.h"
 #include "RooRealVar.h"
 #include "RooConstVar.h"
-#include "RooBreitWigner.h"
+#include "RooGaussian.h"
 // Custom headers
 #include "plotmaker.h"
-
-void GetResolution(string filename, string particlename, string branchname, string xtitle, string unit, string plotname, double xlow, double xup, int nbins)
+#include "progbar.h"
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+void GetResolution(string filename, vector<string> particlename, string branchname, string xtitle, string unit, string plotname, double xlow, double xup, int nbins)
 {
 /*Input***********************************************************************/
   TFile* file = new TFile(filename.c_str());
@@ -37,71 +43,105 @@ void GetResolution(string filename, string particlename, string branchname, stri
     cout << "Couldn't find tree." << endl;
     return;
   }
+  cout << "Tree found." << endl;
 /*Construct resolution********************************************************/
-  TTree* newtree = new TTree();
-  double res; newtree->Branch("res",&res,"res/D");
   // KK_TRUEP branches are identical to B_s0_TRUEP (bug?)
   // phi_1020_TRUEP is fine, so are individual kaons
-  TLorentzVector TRUEP[2];
-  double TRUEP_E[2]; tree->SetBranchAddress("Kplus0_TRUEP_E",&TRUEP_E[0]); tree->SetBranchAddress("Kminus0_TRUEP_E",&TRUEP_E[1]);
-  double TRUEP_X[2]; tree->SetBranchAddress("Kplus0_TRUEP_X",&TRUEP_X[0]); tree->SetBranchAddress("Kminus0_TRUEP_X",&TRUEP_X[1]);
-  double TRUEP_Y[2]; tree->SetBranchAddress("Kplus0_TRUEP_Y",&TRUEP_Y[0]); tree->SetBranchAddress("Kminus0_TRUEP_Y",&TRUEP_Y[1]);
-  double TRUEP_Z[2]; tree->SetBranchAddress("Kplus0_TRUEP_Z",&TRUEP_Z[0]); tree->SetBranchAddress("Kminus0_TRUEP_Z",&TRUEP_Z[1]);
-  double SIMMASS; tree->SetBranchAddress((particlename+branchname).c_str(),&SIMMASS);
+  int n = particlename.size();
+  TLorentzVector TRUEP[n+1];
+  double TRUEP_E[n];
+  double TRUEP_X[n];
+  double TRUEP_Y[n];
+  double TRUEP_Z[n];
+  for(int i = 0; i < n; i++)
+  {
+     tree->SetBranchAddress((particlename[i]+"_TRUEP_E").c_str(),&TRUEP_E[i]);
+     tree->SetBranchAddress((particlename[i]+"_TRUEP_X").c_str(),&TRUEP_X[i]);
+     tree->SetBranchAddress((particlename[i]+"_TRUEP_Y").c_str(),&TRUEP_Y[i]);
+     tree->SetBranchAddress((particlename[i]+"_TRUEP_Z").c_str(),&TRUEP_Z[i]);
+  }
+  double SIMMASS; tree->SetBranchAddress(branchname.c_str(),&SIMMASS);
+  // Output tree and branches
+  TTree* newtree = new TTree();
+  double res ; newtree->Branch("res" ,&res ,"res/D" );
+  double mass; newtree->Branch("mass",&mass,"mass/D");
+  cout << "Constructing resolution for " << n << "-body invariant mass" << endl;
+  progbar bar(tree->GetEntries());
   for(int i = 0; i < tree->GetEntries(); i++)
   {
     tree->GetEntry(i);
-    TRUEP[0].SetPxPyPzE(TRUEP_X[0],TRUEP_Y[0],TRUEP_Z[0],TRUEP_E[0]);
-    TRUEP[1].SetPxPyPzE(TRUEP_X[1],TRUEP_Y[1],TRUEP_Z[1],TRUEP_E[1]);
-    res = (TRUEP[0]+TRUEP[1]).M() - SIMMASS;
+    TRUEP[n].SetPxPyPzE(0,0,0,0);
+    for(int j = 0; j < n; j++)
+    {
+      TRUEP[j].SetPxPyPzE(TRUEP_X[j],TRUEP_Y[j],TRUEP_Z[j],TRUEP_E[j]);
+      TRUEP[n] = TRUEP[n]+TRUEP[j];
+    }
+    mass = TRUEP[n].M();
+    res = mass - SIMMASS;
+    if(i%(tree->GetEntries()/100)==0)
+    {
+      bar.print(i);
+    }
+    if(res < xlow || res > xup) continue;
     newtree->Fill();
   }
+  bar.terminate();
 /*Plot************************************************************************/
   using namespace RooFit;
   RooRealVar* x = new RooRealVar("res",xtitle.c_str(),xlow,xup);
   RooDataSet* data;
-  std::cout << "Importing tree" << endl;
+  cout << "Importing tree" << endl;
   data = new RooDataSet("data","",RooArgSet(*x),Import(*newtree));
   RooRealVar mean("mean","",0,-10,10);
   RooRealVar sigma("sigma","mass resolution",20,0,100);
-  RooBreitWigner* model = new RooBreitWigner("model","",*x,mean,sigma);
+  RooGaussian* model = new RooGaussian("model","",*x,mean,sigma);
   model->fitTo(*data);
   RooPlot* frame = x->frame();
-  std::cout << "Plotting" << endl;
+  cout << "Plotting" << endl;
   data->plotOn(frame,Binning(nbins));
   model->plotOn(frame);
   frame->SetMaximum(frame->GetMaximum()*1.3);
   plotmaker plotter(frame);
   plotter.SetTitle(("#Delta"+xtitle), unit);
   plotter.Draw()->SaveAs((plotname+".pdf").c_str());
+  TCanvas can;
+  newtree->Draw("res:mass");
+  can.SaveAs("2Dscatter.pdf");
 }
 /*Main function***************************************************************/
 int main(int argc, char* argv[])
 {
   using namespace boost::program_options;
   options_description desc("Allowed options",120);
-  std::string file, partic, branch, cuts, xtitle, unit, plot, weight;
+  string file, branch, cuts, xtitle, unit, plot, weight;
+  vector<string> partic;
   double xlow, xup;
   int nbins;
   desc.add_options()
-    ("help,H"    ,                                                                                      "produce help message"                      )
-    ("file,F"    , value<std::string>(&file  )->default_value("ntuples/BsphiKK_MC_mvacut.root"       ), "set data file"                             )
-    ("particle,P", value<std::string>(&partic)->default_value("KK"                                   ), "set particle name"                         )
-    ("branch,B"  , value<std::string>(&branch)->default_value("_M"                                   ), "set branch to compare to"                  )
-    ("title,T"   , value<std::string>(&xtitle)->default_value("#it{m}(#it{K^{#plus}K^{#minus}})"     ), "set x-axis title (takes ROOT LaTeX format)")
-    ("unit,U"    , value<std::string>(&unit  )->default_value("MeV/#it{c}^{2}"                       ), "set unit (takes ROOT LaTeX format)"        )
-    ("plot,O"    , value<std::string>(&plot  )->default_value("massresolution"                       ), "set output plot filename"                  )
-    ("upper,u"   , value<double     >(&xup   )->default_value(50                                     ), "set plot upper limit"                      )
-    ("lower,l"   , value<double     >(&xlow  )->default_value(-50                                    ), "set plot lower limit"                      )
-    ("bins,b"    , value<int        >(&nbins )->default_value(50                                     ), "set number of bins"                        )
+    ("help,H"    ,                                                                                    "produce help message"                  )
+    ("file,F"    , value<string        >(&file  )->default_value("ntuples/BsphiKK_MC_mvacut.root"  ), "data ntuple"                           )
+    ("particle,P", value<vector<string>>(&partic)->multitoken()                                     , "list particles to sum"                 )
+    ("branch,B"  , value<string        >(&branch)->default_value("KK_M"                            ), "mass branch to compare to"             )
+    ("title,T"   , value<string        >(&xtitle)->default_value("#it{m}(#it{K^{#plus}K^{#minus}})"), "x-axis title (takes ROOT LaTeX format)")
+    ("unit,U"    , value<string        >(&unit  )->default_value("MeV/#it{c}^{2}"                  ), "x-axis unit (takes ROOT LaTeX format)" )
+    ("plot,O"    , value<string        >(&plot  )->default_value("massresolution"                  ), "output plot filename"                  )
+    ("upper,u"   , value<double        >(&xup   )->default_value(50                                ), "x-axis upper limit"                    )
+    ("lower,l"   , value<double        >(&xlow  )->default_value(-50                               ), "x-axis lower limit"                    )
+    ("bins,b"    , value<int           >(&nbins )->default_value(50                                ), "number of bins"                        )
   ;
   variables_map vmap;
   store(parse_command_line(argc, argv, desc), vmap);
   notify(vmap);
   if (vmap.count("help"))
   {
-    std::cout << desc << endl;
+    cout << desc << endl;
     return 1;
+  }
+//  partic = vmap["particle"].as<vector<string>>();
+  if(partic.size() == 0)
+  {
+    partic.push_back("Kplus0");
+    partic.push_back("Kminus0");
   }
   cout << "Entering main function" << endl;
   GetResolution(file,partic,branch,xtitle,unit,plot,xlow,xup,nbins);
