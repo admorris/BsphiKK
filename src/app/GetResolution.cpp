dd9a0b70c85f0f978dcb558509a6dd9c96987f88
Adam Morris <adam.morris@cern.ch>
2016-01-22 10:35:22 +0100

    fit mKK resolution with breit wigner

--- src/app/GetResolution.cpp
+++ src/app/GetResolution.cpp
@@ -10,11 +10,14 @@
 #include "RooDataSet.h"
 #include "RooPlot.h"
 #include "RooRealVar.h"
+#include "RooConstVar.h"
+#include "RooBreitWigner.h"
 // Custom headers
 #include "plotmaker.h"
 
 void GetResolution(string filename, string particlename, string branchname, string xtitle, string unit, string plotname, double xlow, double xup, int nbins)
 {
+/*Input***********************************************************************/
   TFile* file = new TFile(filename.c_str());
   TTree* tree;
   if((TTree*)file->Get("DecayTreeTuple/DecayTree") != (TTree*)0x0)
@@ -34,38 +37,45 @@ void GetResolution(string filename, string particlename, string branchname, stri
     cout << "Couldn't find tree." << endl;
     return;
   }
+/*Construct resolution********************************************************/
   TTree* newtree = new TTree();
-  double res; tree->Branch("res",&res,"res/D");
-  TLorentzVector TRUEP;
-  double TRUEP_E; tree->SetBranchAddress((particlename+"_TRUEP_E").c_str(),&TRUEP_E);
-  double TRUEP_X; tree->SetBranchAddress((particlename+"_TRUEP_X").c_str(),&TRUEP_X);
-  double TRUEP_Y; tree->SetBranchAddress((particlename+"_TRUEP_Y").c_str(),&TRUEP_Y);
-  double TRUEP_Z; tree->SetBranchAddress((particlename+"_TRUEP_Z").c_str(),&TRUEP_Z);
-  double SIMMASS; tree->SetBranchAddress(branchname.c_str(),&SIMMASS);
+  double res; newtree->Branch("res",&res,"res/D");
+  // KK_TRUEP branches are identical to B_s0_TRUEP (bug?)
+  // phi_1020_TRUEP is fine, so are individual kaons
+  TLorentzVector TRUEP[2];
+  double TRUEP_E[2]; tree->SetBranchAddress("Kplus0_TRUEP_E",&TRUEP_E[0]); tree->SetBranchAddress("Kminus0_TRUEP_E",&TRUEP_E[1]);
+  double TRUEP_X[2]; tree->SetBranchAddress("Kplus0_TRUEP_X",&TRUEP_X[0]); tree->SetBranchAddress("Kminus0_TRUEP_X",&TRUEP_X[1]);
+  double TRUEP_Y[2]; tree->SetBranchAddress("Kplus0_TRUEP_Y",&TRUEP_Y[0]); tree->SetBranchAddress("Kminus0_TRUEP_Y",&TRUEP_Y[1]);
+  double TRUEP_Z[2]; tree->SetBranchAddress("Kplus0_TRUEP_Z",&TRUEP_Z[0]); tree->SetBranchAddress("Kminus0_TRUEP_Z",&TRUEP_Z[1]);
+  double SIMMASS; tree->SetBranchAddress((particlename+branchname).c_str(),&SIMMASS);
   for(int i = 0; i < tree->GetEntries(); i++)
   {
     tree->GetEntry(i);
-    TRUEP.SetPxPyPzE(TRUEP_X,TRUEP_Y,TRUEP_Z,TRUEP_E);
-    res = TRUEP.M() - SIMMASS;
+    TRUEP[0].SetPxPyPzE(TRUEP_X[0],TRUEP_Y[0],TRUEP_Z[0],TRUEP_E[0]);
+    TRUEP[1].SetPxPyPzE(TRUEP_X[1],TRUEP_Y[1],TRUEP_Z[1],TRUEP_E[1]);
+    res = (TRUEP[0]+TRUEP[1]).M() - SIMMASS;
     newtree->Fill();
   }
-  
-  
-  
+/*Plot************************************************************************/
   using namespace RooFit;
   RooRealVar* x = new RooRealVar("res",xtitle.c_str(),xlow,xup);
   RooDataSet* data;
   std::cout << "Importing tree" << endl;
   data = new RooDataSet("data","",RooArgSet(*x),Import(*newtree));
+  RooRealVar mean("mean","",0,-10,10);
+  RooRealVar sigma("sigma","mass resolution",20,0,100);
+  RooBreitWigner* model = new RooBreitWigner("model","",*x,mean,sigma);
+  model->fitTo(*data);
   RooPlot* frame = x->frame();
   std::cout << "Plotting" << endl;
   data->plotOn(frame,Binning(nbins));
+  model->plotOn(frame);
   frame->SetMaximum(frame->GetMaximum()*1.3);
   plotmaker plotter(frame);
-  plotter.SetTitle(xtitle, unit);
+  plotter.SetTitle(("#Delta"+xtitle), unit);
   plotter.Draw()->SaveAs((plotname+".pdf").c_str());
 }
-
+/*Main function***************************************************************/
 int main(int argc, char* argv[])
 {
   using namespace boost::program_options;
@@ -77,12 +87,12 @@ int main(int argc, char* argv[])
     ("help,H"    ,                                                                                      "produce help message"                      )
     ("file,F"    , value<std::string>(&file  )->default_value("ntuples/BsphiKK_MC_mvacut.root"       ), "set data file"                             )
     ("particle,P", value<std::string>(&partic)->default_value("KK"                                   ), "set particle name"                         )
-    ("branch,B"  , value<std::string>(&branch)->default_value("_LOKI_Mass"                           ), "set branch to compare to"                  )
+    ("branch,B"  , value<std::string>(&branch)->default_value("_M"                                   ), "set branch to compare to"                  )
     ("title,T"   , value<std::string>(&xtitle)->default_value("#it{m}(#it{K^{#plus}K^{#minus}})"     ), "set x-axis title (takes ROOT LaTeX format)")
     ("unit,U"    , value<std::string>(&unit  )->default_value("MeV/#it{c}^{2}"                       ), "set unit (takes ROOT LaTeX format)"        )
     ("plot,O"    , value<std::string>(&plot  )->default_value("massresolution"                       ), "set output plot filename"                  )
-    ("upper,u"   , value<double     >(&xup   )->default_value(10                                     ), "set plot upper limit"                      )
-    ("lower,l"   , value<double     >(&xlow  )->default_value(-10                                    ), "set plot lower limit"                      )
+    ("upper,u"   , value<double     >(&xup   )->default_value(50                                     ), "set plot upper limit"                      )
+    ("lower,l"   , value<double     >(&xlow  )->default_value(-50                                    ), "set plot lower limit"                      )
     ("bins,b"    , value<int        >(&nbins )->default_value(50                                     ), "set number of bins"                        )
   ;
   variables_map vmap;
