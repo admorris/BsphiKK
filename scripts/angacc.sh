d68320e3dd871a8f1a11299d22837e8ea01f7a16
Adam Morris <adam.morris@cern.ch>
2016-02-19 17:22:29 +0100

    don't need another log file..

--- scripts/angacc.sh
+++ scripts/angacc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../fits
-fitting -f acceptance.xml --calculateAcceptanceCoefficients | tee acceptance.log
+fitting -f acceptance.xml --calculateAcceptanceCoefficients
 mv LegendreMoments.root LegendreMoments_Acceptance.root
 ../bin/PlotAngAcc sampled_acceptance.root acceptance.root
 mv acceptance_*pdf ../latex/figs/
