2346d0062518a460040b5427419bebbbfa551082
Adam Morris <adam.morris@cern.ch>
2016-02-19 18:28:02 +0100

    typo

--- scripts/bkgparam.sh
+++ scripts/bkgparam.sh
@@ -10,7 +10,7 @@ cd ../ntuples
                  -M Threshold \
                  -B KK_M \
                  -T "#it{m}(#it{K^{#plus}K^{#minus}})" \
-                 -U "MeV/#it{c}^{2})" \
+                 -U "MeV/#it{c}^{2}" \
                  -C "TMath::Abs(B_s0_LOKI_Mass-5366.77)>50" \
                  -l 900 \
                  -u 1800 \
