b88a6a4ee95aedc99913bba96f416c15c2cfb6f3
Adam Morris <adam.morris@cern.ch>
2016-02-19 15:14:38 +0100

    better comments

--- scripts/ANALYSIS.sh
+++ scripts/ANALYSIS.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-# A script to keep track of all the other scripts. Assumes the DaVinci output is available on the grid. If not, see the folder gridjobs.
-# Please run all scripts from within the scripts folder
-#./getoutputdata | tee log/getoutputdata.log
-#./merge.sh | tee log/merge.log
-##./docuts.sh | tee log/docuts.log
-#./addBranches.sh | tee log/addBranches.log
-#./dovetoes.sh | tee log/dovetoes.log
-#./showvetoes.sh | tee log/showvetoes.log
-#./vetoeff.sh | tee log/vetoeff.log
-#./seleff.sh
-#./trigeff.sh
+## A script to keep track of all the other scripts. Assumes the DaVinci output is available on the grid. If not, see the folder gridjobs.
+## Please run all scripts from within the scripts folder
+##
+## Get DaVinci output
+./getoutputdata | tee log/getoutputdata.log
+./merge.sh | tee log/merge.log
+## Cut-based selection
+./docuts.sh | tee log/docuts.log
+./addBranches.sh | tee log/addBranches.log
+./dovetoes.sh | tee log/dovetoes.log
+./showvetoes.sh | tee log/showvetoes.log
+## Efficiencies
+./vetoeff.sh | tee log/vetoeff.log
+./seleff.sh
+./trigeff.sh
+## Preliminary m(KK) plots
 ./sweight.sh | tee log/sweight.log
 ./plotMKK.sh
-#./mvadataMCsWcomp.sh | tee log/mvadataMCsWcomp.log
-#./trainmva.sh | tee log/trainmva.log
+## Multivariate selection
+./mvadataMCsWcomp.sh | tee log/mvadataMCsWcomp.log
+./trainmva.sh | tee log/trainmva.log
 ./applymva.sh | tee log/applymva.log
 ./mvaplots.sh | tee log/mvaplots.log
 ./mvadataMCsWcompOutput.sh | tee log/mvadataMCsWcompOutput.log
 ./mvacut.sh | tee log/mvacut.log
 ./mvacuteff.sh | tee log/mvacuteff.log
+## Plot fully-selected m(KK) and helicity angles
 ./plotmKK_mva.sh | tee log/plotmKK_mva.log
 ./plot4D.sh | tee log/plot4D.log
+## Get input parameters for angular fit
 ./doBsmassfit.sh | tee log/Bsmassfit.log
 ./angacc.sh | tee log/angacc.log
+./bkgparam.sh | tee log/bkgparam.log
