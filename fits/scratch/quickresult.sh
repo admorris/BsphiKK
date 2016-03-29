5a6901ae15295ba7373eb7cd2ed03364089315ec
Adam Morris <adam.morris@cern.ch>
2016-03-29 15:11:25 +0200

    ensure last folder and quieter latex output

--- fits/scratch/quickresult.sh
+++ fits/scratch/quickresult.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-recentdir=$(find . -type d -name "RapidFitOutput_*" | tail -1)
-find $recentdir -type f -name "Overlay_*_All_Data.pdf" -exec cp {} ./slides \;
+recentdir=$(find . -type d -name "RapidFitOutput_*" | sort | tail -1)
+find $recentdir -type f -name "Overlay_*_All_Data.pdf" -exec cp -v {} ./slides \;
 cd slides
-pdflatex slide.tex
+pdflatex -interaction=batchmode slide.tex
 cd ..
