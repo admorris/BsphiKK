e764f33cb12be9adae19a470b550d44bf932084a
Adam Morris <adam.morris@cern.ch>
2016-04-07 16:51:25 +0200

    at least ganga doesn't crash now

--- gridjobs/DaVinci/GenLvl/Ganga-DaVinci.py
+++ gridjobs/DaVinci/GenLvl/Ganga-DaVinci.py
@@ -1,15 +1,18 @@
-# Does not immediately work on any current version of Ganga (6.1.11 or before)
-# It should hopefully work with the next version
-# https://github.com/ganga-devs/ganga/commit/30e0ef56537ce8f5b2ac6b89a9666cd8024b137b
-JobName = 'BsPhiKKGen'
+JobName  = 'BsPhiPhiGen'
+LFNsFile = 'PwaveLFNs.txt'
+#JobName  = 'BsPhiKKGen'
+#LFNsFile = 'PhaseSpaceLFNs.txt'
 Application = DaVinci()
 Application.version = 'v37r0'
 
-Splitter = SplitByFiles(filesPerJob = 20, maxFiles = -1, ignoremissing = True, bulksubmit=True)
+Splitter = SplitByFiles(ignoremissing = True, bulksubmit=True)
 Output   = [ DiracFile('*.root'), LocalFile('summary.xml') ]
 Backend  = Dirac()
-#Input    = [ File ( './LFNs.py' ) ]
-Options = [ File ( './tupleResult.py' ), File ( './PwaveLFNs.py' ) ] # This isn't the proper way to do this
+Input    = []
+LFNs = [line.rstrip('\n') for line in open(LFNsFile)]
+for line in LFNs:
+  Input.append(DiracFile(lfn=line))
+Options  = [ File ( './tupleResult.py' ) ]
 Application.optsfile = Options
 j = Job ()
 j.name         = JobName
@@ -17,7 +20,7 @@ j.application  = Application
 j.splitter     = Splitter
 j.backend      = Backend
 j.outputfiles  = Output
-#j.inputdata    = j.application.readInputData(Options) # This crashes for some obscure reason.
+j.inputdata    = Input
 j.do_auto_resubmit = True
 j.prepare()
 j.submit()
