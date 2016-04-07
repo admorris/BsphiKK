0314d9d047227833c4b1cd368616a701eec50595
Adam Morris <adam.morris@cern.ch>
2016-04-07 16:57:04 +0200

    don't need these

--- gridjobs/Gauss/PwaveMC/runlocal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gaudirun.py $GAUSSOPTS/Gauss-2012.py \
-            13104013.py \
-            $LBPYTHIAROOT/options/Pythia.py \
-            $GAUSSOPTS/GenStandAlone.py \
-            Gauss-Job.py
