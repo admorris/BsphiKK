1aeb1ce3326a6cc1d677abb690be372470f14d0a
Adam Morris <adam.morris@cern.ch>
2016-04-19 14:37:27 +0200

    zoom

--- scripts/showvetoes.sh
+++ scripts/showvetoes.sh
@@ -1,8 +1,8 @@
 suffices=(_mvaVars _mvaVars_vetoes)
 branches=(phiKplusM phiKminusM phipipiM pipiM phipiplusM phipiminusM phiKpiM KpiM phipM phipbarM phiKpM KpM)
 titles=("#it{#phi K}^{#plus}" "#it{#phi K}^{#minus}" "#it{#phi}#it{#pi}^{#plus}#it{#pi}^{#minus}" "#it{#pi}^{#plus}#it{#pi}^{#minus}" "#it{#phi}#it{#pi}^{#plus}" "#it{#phi}#it{#pi}^{#minus}" "#it{#phi K #pi}" "#it{K #pi}" "#it{#phi}#it{p}" "#it{#phi}#it{#bar{p}}" "#it{#phi K p}" "#it{K p}")
-lower=(1500 1500 4800 250 1500 1500 5000 600 1500 1500 5200 1400)
-upper=(3000 3000 5500 4000 3000 3000 5600 4000 3000 3000 6500 4500)
+lower=(1500 1500 4800 250 1500 1500 5000 600 1500 1500 5500 1400)
+upper=(3000 3000 5500 4000 3000 3000 5600 4000 3000 3000 6000 4500)
 for i in `seq 0 11`
 do
   for suffix in ${suffices[@]}
