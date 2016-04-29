3f7dfc6aadac467e2f1d69c0f0f734ced3e3be68
Adam Morris <adam.morris@cern.ch>
2016-04-29 19:41:36 +0200

    nicer plots

--- src/app/PlotAngAcc.cpp
+++ src/app/PlotAngAcc.cpp
@@ -2,6 +2,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TCanvas.h"
+#include "TLatex.h"
 #include "TPad.h"
 #include "TH1D.h"
 #include "TH2F.h"
@@ -31,29 +32,63 @@ void PlotAngAcc(string filename,string plotfilename)
                         , "cos #it{#theta_{1}}"
                         , "cos #it{#theta_{2}}"
                         };
+  string unit[4] = { " MeV/#it{c}^{2}", "", "", "" };
   map<string,string> titlemap;
-  float textsize = 0.05;
+  float textsize = 0.055;
   for(int i = 0; i < 4; i++)
   {
     titlemap[varname[i]] = axistitle[i];
     pad = (TPad*)can->GetPrimitive(("acc_can_"+itoa(i+1)).c_str());
     AccData = (TH1D*)pad->GetPrimitive((histname[i]+"Acc"    ).c_str());
     AccProj = (TH1D*)pad->GetPrimitive((histname[i]+"AccProj").c_str());
-    canv1d = new TCanvas();
+    canv1d = new TCanvas("canvas","",1200,900);
     canv1d->cd();
+    canv1d->SetFillColor(kWhite);
     pad = new TPad();
-    pad->SetMargin(0.05,0.05,0.05,0.2);
+    // left right bottom top
+    pad->SetMargin(0.15,0.05,0.15,0.05);
+    pad->SetTicks(1,1);
+    pad->SetFillColor(kWhite);
     pad->Draw();
     pad->cd();
+    AccData = new TH1D(*AccData);
     AccData->SetMinimum(0);
     AccData->SetTitle("");
     AccData->GetXaxis()->SetTitle(axistitle[i].c_str());
-    AccData->Draw();
-    AccProj->Draw("same");
-    AccData->GetXaxis()->SetTitleSize(textsize);
+    char ytitle[70];
+    sprintf(ytitle,"Candidates / (%.1f%s)",AccData->GetBinWidth(1),unit[i].c_str());
+    AccData->SetMaximum(1.3*AccData->GetMaximum());
+    AccData->GetYaxis()->SetTitle(ytitle);
+    AccData->SetLineColor(kBlack);
+    AccData->SetLineWidth(1);
+    AccData->SetMarkerSize(1);
+    AccData->SetMarkerStyle(8);
+    AccData->SetMarkerColor(kBlack);
+    AccData->Draw("E1");
+    AccProj = new TH1D(*AccProj);
+    AccProj->SetLineColor(kRed);
+    AccProj->SetLineWidth(2);
+    AccProj->SetMarkerSize(0);
+    AccProj->Draw("hist same c");
+    AccData->GetXaxis()->SetTitleSize(textsize*1.2);
+    AccData->GetYaxis()->SetTitleSize(textsize*1.2);
     AccData->GetXaxis()->SetLabelSize(textsize);
-    AccData->GetXaxis()->CenterTitle();
     AccData->GetYaxis()->SetLabelSize(textsize);
+    AccData->GetXaxis()->SetTitleFont(132);
+    AccData->GetYaxis()->SetTitleFont(132);
+    AccData->GetXaxis()->SetLabelFont(132);
+    AccData->GetYaxis()->SetLabelFont(132);
+    AccData->GetYaxis()->CenterTitle();
+    double _blurbx        = 0.75;
+    double _blurby        = 0.80;
+    string _blurbtext     = "#splitline{LHCb}{#scale[0.75]{Preliminary}}";
+    TLatex* _blurb = new TLatex(_blurbx,_blurby,_blurbtext.c_str());
+    _blurb->SetTextFont(132);
+    _blurb->SetTextColor(1);
+    _blurb->SetNDC(kTRUE);
+    _blurb->SetTextAlign(11);
+    _blurb->SetTextSize(0.07);
+    _blurb->Draw();
     canv1d->SaveAs((head+"_"+varname[i]+"_proj.pdf").c_str());    
   }
   
