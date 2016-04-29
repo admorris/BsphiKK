4b29a309b48260a0c6d7f1ebba1e4bac7b9a88fa
Adam Morris <adam.morris@cern.ch>
2016-04-29 19:46:27 +0200

    opts

--- scripts/doBsmassfit.sh
+++ scripts/doBsmassfit.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 cd ../ntuples/
 ../bin/BsMassFit \
-    -M ../ntuples/BsphiKK_MC_mvacut.root \
-    -R ../ntuples/BsphiKK_data_mvacut.root \
+    -M BsphiKK_MC_mvacut.root \
+    -R BsphiKK_data_mvacut.root \
     -O ../latex/figs/Bsmassfit_mvacut \
     -N B_s0_LOKI_Mass \
     --pulls
 
 ../bin/BsMassFit \
-    -M ../ntuples/BsphiKK_MC_1050_mvacut.root \
-    -R ../ntuples/BsphiKK_data_1050_mvacut.root \
+    -M BsphiKK_MC_1050_mvacut.root \
+    -R BsphiKK_data_1050_mvacut.root \
     -O ../latex/figs/Bsmassfit_1050_mvacut \
     -N B_s0_LOKI_Mass \
     --pulls
 
 ../bin/BsMassFit \
-    -M ../ntuples/BsphiKK_MC_1800_mvacut.root \
-    -R ../ntuples/BsphiKK_data_1800_mvacut.root \
+    -M BsphiKK_MC_1800_mvacut.root \
+    -R BsphiKK_data_1800_mvacut.root \
     -O ../latex/figs/Bsmassfit_1800_mvacut \
     -N B_s0_LOKI_Mass \
     --pulls \
-    --draw-region 2
+    --draw-region 2 \
+    --backgrounds ../f1420_Bs0_branches.root/HISTPDF BdphiKst_MC_mvacut.root LbphiKp_MC_mvacut.root \
+    --yields 0 0.02 475 \
+    --yopts flo flo abs \
+    --logy
 
 ../bin/BsMassFit \
-    -M ../ntuples/BsphiKK_MC_1050_1800_mvacut.root \
-    -R ../ntuples/BsphiKK_data_1050_1800_mvacut.root \
+    -M BsphiKK_MC_1050_1800_mvacut.root \
+    -R BsphiKK_data_1050_1800_mvacut.root \
     -O ../latex/figs/Bsmassfit_1050_1800_mvacut \
     -N B_s0_LOKI_Mass \
     --pulls
