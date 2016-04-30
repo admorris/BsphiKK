aea3d4631fb10917ec659aa69ae8e912288a23c5
Adam Morris <adam.morris@cern.ch>
2016-04-30 23:31:55 +0200

    ignore Gen level files

--- scripts/dovetoes.sh
+++ scripts/dovetoes.sh
@@ -3,7 +3,7 @@ source cuts.sh
 cd ../ntuples
 echo $phiwindow
 ################################################################################
-for mode in $(ls *_mvaVars.root | sed 's/_mvaVars\.root//'); do
+for mode in $(ls *data*_mvaVars.root *MC*_mvaVars.root | sed 's/_mvaVars\.root//'); do
 cutapplier ${mode}_mvaVars.root DecayTree "${phikstveto}&&${LbphiKpveto}&&${Lcphipveto}&&${DtoKaonsveto}&&${Dtophipiveto}" ${mode}_mvaVars_vetoes.root
 done
 exit 0
