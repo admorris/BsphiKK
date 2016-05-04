8b6fc57c6c9bb4b90d9eb24657c95bbdb6a3f1ea
Adam Morris <adam.morris@cern.ch>
2016-05-04 19:28:32 +0200

    new syntax and forking

--- scripts/addBranches.sh
+++ scripts/addBranches.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ../ntuples/
 for mode in $(ls *_cuts.root | sed 's/_cuts\.root//'); do
-../bin/AddBranches ${mode}
+../bin/AddBranches ${mode}_cuts.root ${mode}_mvaVars.root &
 done
+wait
 exit 0
