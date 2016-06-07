3cb2e57a2ec399968e8b1834a1622761b4c8ab2f
Adam Morris <adam.morris@cern.ch>
2016-06-07 12:00:01 +0100

    readability

--- scripts/mvacuteff.sh
+++ scripts/mvacuteff.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd ../ntuples/
-../bin/BsMassFit -M ../ntuples/Bsphiphi_MC_mva.root -R ../ntuples/BsphiKK_data_mva.root -C "TMath::Abs(KK_M-1019.461)<15" -O ../latex/figs/Bsphiphi_fit_nomva --pulls
-../bin/BsMassFit -M ../ntuples/Bsphiphi_MC_mvacut.root -R ../ntuples/BsphiKK_data_mvacut.root -C "TMath::Abs(KK_M-1019.461)<15" -O ../latex/figs/Bsphiphi_fit_mvacut --pulls
+../bin/BsMassFit -M ../ntuples/Bsphiphi_MC_mva.root \
+                 -R ../ntuples/BsphiKK_data_mva.root -C "TMath::Abs(KK_M-1019.461)<15" \
+                 -O ../latex/figs/Bsphiphi_fit_nomva \
+                 --pulls
+../bin/BsMassFit -M ../ntuples/Bsphiphi_MC_mvacut.root \
+                 -R ../ntuples/BsphiKK_data_mvacut.root \
+                 -C "TMath::Abs(KK_M-1019.461)<15" \
+                 -O ../latex/figs/Bsphiphi_fit_mvacut \
+                 --pulls
