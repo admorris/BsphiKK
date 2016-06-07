8018dc7b62393a36a43f292b2af922bf704d3174
Adam Morris <adam.morris@cern.ch>
2016-06-07 18:49:41 +0200

    little thing to calculate efficiency and add it to results table

---
+++ src/app/CalcEff.cpp
@@ -0,0 +1,17 @@
+#include "ResultDB.h"
+
+int main(int argc, char* argv[])
+{
+  if(argc != 5)
+  {
+    printf("Usage: %s <results.csv> <numerator> <denominator> <new name>",argv[0]);
+    return 1;
+  }
+  ResultDB rdb(argv[1]);
+  result numerator = rdb.Get(argv[2]);
+  result denominator = rdb.Get(argv[3]);
+  double value = numerator.value/denominator.value;
+  double error = sqrt(pow(numerator.error/numerator.value,2)+pow(denominator.error/denominator.value,2));
+  rdb.Update(argv[4],"percent",value,error);
+  return 0;
+}
