e7a804117593bbc2ebbe82229bf093d00b3c589a
Adam Morris <adam.morris@cern.ch>
2016-06-15 20:54:11 +0100

    maybe this works, I don't know

--- gridjobs/DaVinci/AllnTuples/Ganga-DaVinci.py
+++ gridjobs/DaVinci/AllnTuples/Ganga-DaVinci.py
@@ -4,9 +4,10 @@ os.system("sed -i 's/IsMC = True/IsMC = False/g' DaVinci-Opts_12.py")
 os.system("cp DaVinci-Opts_12.py DaVinci-Opts_11.py")
 os.system("sed -i 's/= \"2012\"/= \"2011\"/g' DaVinci-Opts_11.py")
 
+
+
 DaVinciVersion = 'v36r1'
-myJobName = ['Bsphiphi', 'BsphiKK', 'BdphiKst', 'Bsphipipi', 'LbphiKp', 'phiKK_11', 'phiKK_12']
-myFile = ['./DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts_11.py', './DaVinci-Opts_12.py']
+
 myApplication = DaVinci()
 myApplication.version = DaVinciVersion
 myApplication.user_release_area = '/afs/cern.ch/user/a/admorris/cmtuser'
@@ -18,30 +19,35 @@ myOutput = [ DiracFile('BsphiKK.root'), DiracFile('dummy.root'), LocalFile('summ
 myBackend = Dirac()
 dirc = '~/work/BsphiKK/gridjobs/'
 magnet = ['Down', 'Up']
-InputName = []
+myFile = ['./DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', './DaVinci-Opts.py', , './DaVinci-Opts_12.py']
 # Bs to phi phi
+InputName = []
 for i in range (2):
     dataName = 'MC201213104013Beam4000GeV-2012-Mag' + magnet[i] + '-Nu2.5-Pythia8Sim08aDigi13Trig0x409f0045Reco14aStripping20NoPrescalingFlaggedSTREAMSDST.py'
-    InputName.append(dataName)
+    submit_job(name = "Bsphiphi", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 # Bs to phi KK
+InputName = []
 for i in range (2):
     dataName = 'MC201213104024Beam4000GeV-2012-Mag' + magnet[i] + '-Nu2.5-Pythia8Sim08aDigi13Trig0x409f0045Reco14aStripping20NoPrescalingFlaggedSTREAMSDST.py'
-    InputName.append(dataName)
+    submit_job(name = "BsphiKK", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 for i in range (2):
     dataName = 'MC201213104024Beam4000GeV-2012-Mag' + magnet[i] + '-Nu2.5-Pythia8Sim08iDigi13Trig0x409f0045Reco14cStripping20NoPrescalingFlaggedALLSTREAMSDST.py'
-    InputName.append(dataName)
+    submit_job(name = "BsphiKK", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 # Bd to phi K*
+InputName = []
 for i in range (2):
     dataName = 'MC201211104021Beam4000GeV-2012-Mag' + magnet[i] + '-Nu2.5-Pythia8Sim08aDigi13Trig0x409f0045Reco14aStripping20NoPrescalingFlaggedALLSTREAMS.DST.py'
-    InputName.append(dataName)
+    submit_job(name = "BdphiKst", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 # Bs to phi pi pi
+InputName = []
 for i in range (2):
     dataName = 'MC201213104081Beam4000GeV-2012-Mag' + magnet[i] + '-Nu-2.5-Pythia8Sim08eDigi13Trig0x409f0045Reco14aStripping20NoPrescalingFlaggedALLSTREAMS.DST.py'
-    InputName.append(dataName)
+    submit_job(name = "Bsphipipi", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 # Lb to phi K p
+InputName = []
 for i in range (2):
     dataName = 'MC201215104020Beam4000GeV-2012-Mag' + magnet[i] + '-Nu2.5-Pythia6+8Sim08eDigi13Trig0x409f0045Reco14aStripping20NoPrescalingFlaggedALLSTREAMS.DST.py'
-    InputName.append(dataName)
+    submit_job(name = "LbphiKp", optionsfile = "./DaVinci-Opts.py", DSTLFNs = dataName)
 # Data
 Year = ['11', '12']
 Energy = ['3500', '4000']
@@ -49,23 +55,18 @@ Strip = ['20r1', '20']
 for i in range (2):
     for j in range (2):
         dataName = 'LHCbCollision' + Year[i]+'90000000Beam' + Energy[i] + 'GeV-VeloClosed-Mag' + magnet[j] + 'RealDataReco14Stripping' + Strip[i] + 'BHADRONMDST.py'
-        InputName.append(dataName)
+        submit_job(name = "phiKK_"+Year[i], optionsfile = './DaVinci-Opts_'+Year[i]+'.py', DSTLFNs = dataName)
 
-for i in range (len(InputName) ):
-   print InputName[i]
-   
-if len(InputName) == 2*len(myFile):
-  for nameLoop in range(len(myFile)):
-    for i in range (2):
-      opts_names = [File ( myFile[nameLoop] ) ]
-      opts_names += [File ( dirc + InputName[2*nameLoop + i] ) ]
-      myApplication.optsfile = opts_names
-      j = Job (
-        name         = myJobName[nameLoop],
-        application  = myApplication,
-        splitter     = mySplitter,
-        backend      = myBackend,
-        outputfiles  = myOutput
-      )
-      j.do_auto_resubmit = True
-      j.submit()
+def submit_job(name, optionsfile, DSTLFNs)
+  opts_names = [File ( optionsfile ) ]
+  opts_names += [File ( dirc + DSTLFNs ) ]
+  myApplication.optsfile = opts_names
+  j = Job (
+    name         = name,
+    application  = myApplication,
+    splitter     = mySplitter,
+    backend      = myBackend,
+    outputfiles  = myOutput
+  )
+  j.do_auto_resubmit = True
+  j.submit()
