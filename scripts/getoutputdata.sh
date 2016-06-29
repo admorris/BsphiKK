a28bf68645d9ded78eb8bbc64e4fed55794d7a20
Adam Morris <adam.morris@cern.ch>
2016-06-29 16:11:01 +0200

    for new ntuples

--- scripts/getoutputdata.sh
+++ scripts/getoutputdata.sh
@@ -3,49 +3,56 @@ source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v8r3p1/InstallAre
 source ~/.bash_profile
 source ~/.bashrc
 source /etc/bashrc
-SetupProject.sh LHCbDirac
 lhcb-proxy-init
 ntuple_name='BsphiKK'
 user=admorris
+gangaout=~/gangadir/workspace/${user}/LocalXML/
+lfns='LFNs.txt'
 ###########################################################
 user_fl=${user:0:1}
+source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v8r3p1/InstallArea/scripts/SetupProject.sh Ganga
+for(( jobno=573; jobno<=577; jobno++))
+do
+  if [ "${jobno}" == "575" ]
+  then
+    continue
+  fi
+  cd ${gangaout}
+  ganga getlfns.py ${jobno}
+done
 source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v8r3p1/InstallArea/scripts/SetupProject.sh LHCbDirac
 source /afs/cern.ch/project/eos/installation/lhcb/etc/setup.sh
 if [ ! -d ~/${EOS_HOME}/ ]
 then
-source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount ~/eos
+  source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount ~/eos
 fi
-for(( jobno=481; jobno<=484; jobno++)); do
-#if [ "${jobno}" -gt "424" -a "${jobno}" -lt "429" ]
-if [ "${jobno}" == "482" ]
-then
-continue
-fi
-# Copy outputs to tmp
-gangaout=~/gangadir/workspace/${user}/LocalXML/
-lfns='LFNs.txt'
-cd /tmp/${user}
-mkdir ${jobno}tmp
-cd /tmp/${user}/${jobno}tmp
-find ${gangaout}${jobno} -name "${lfns}" -exec cat {} \; >output_lfns.lst
-n=0
-while read line
+for(( jobno=573; jobno<=577; jobno++))
 do
-if [[ $line == *"${ntuple_name}.root"* ]]
-then
-echo "Retrieving LFN $n: " $line
-dirac-dms-get-file $line
-mv ${ntuple_name}.root ${ntuple_name}${n}_noPID.root
-cutapplier ${ntuple_name}${n}_noPID.root DecayTreeTuple/DecayTree "Kminus_ProbNNk*(1-Kminus_ProbNNpi)>0.01&&Kplus_ProbNNk*(1-Kplus_ProbNNpi)>0.01&&Kminus0_ProbNNk*(1-Kminus0_ProbNNpi)>0.01&&Kplus0_ProbNNk*(1-Kplus0_ProbNNpi)>0.01" ${ntuple_name}${n}_PIDcut.root
-n=$(($n+1))
-fi
-done < output_lfns.lst
-hadd -f ../${ntuple_name}_${jobno}_PIDcut.root ${ntuple_name}*_PIDcut.root
-hadd -f ../${ntuple_name}_${jobno}_noPID.root ${ntuple_name}*_noPID.root
-if [ -e ../${ntuple_name}_${jobno}_PIDcut.root -a -e ../${ntuple_name}_${jobno}_noPID.root ]
-then
-rm ${ntuple_name}*root
-fi
-mv ../${ntuple_name}_${jobno}*.root ~/${EOS_HOME}/phiKK/
+  if [ "${jobno}" == "575" ]
+  then
+    continue
+  fi
+  # Copy outputs to tmp
+  cd /tmp/${user}
+  mkdir ${jobno}tmp
+  cd /tmp/${user}/${jobno}tmp
+  echo ${gangaout}${jobno}/${lfns}
+  cat ${gangaout}${jobno}/${lfns} >output_lfns.lst
+  n=0
+  while read line
+  do
+    echo "Retrieving LFN $n: " $line
+    dirac-dms-get-file $line
+    mv -v ${ntuple_name}.root ${ntuple_name}${n}_noPID.root
+    cutapplier ${ntuple_name}${n}_noPID.root DecayTreeTuple/DecayTree "Kminus_ProbNNk*(1-Kminus_ProbNNpi)>0.01&&Kplus_ProbNNk*(1-Kplus_ProbNNpi)>0.01&&Kminus0_ProbNNk*(1-Kminus0_ProbNNpi)>0.01&&Kplus0_ProbNNk*(1-Kplus0_ProbNNpi)>0.01" ${ntuple_name}${n}_PIDcut.root
+    n=$(($n+1))
+  done < output_lfns.lst
+  hadd -f ../${ntuple_name}_${jobno}_PIDcut.root ${ntuple_name}*_PIDcut.root
+  hadd -f ../${ntuple_name}_${jobno}_noPID.root ${ntuple_name}*_noPID.root
+  if [ -e ../${ntuple_name}_${jobno}_PIDcut.root -a -e ../${ntuple_name}_${jobno}_noPID.root ]
+  then
+    rm -v ${ntuple_name}*root
+  fi
+  mv -v ../${ntuple_name}_${jobno}*.root ~/${EOS_HOME}/phiKK/
 done
 source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse umount ~/eos
--- scripts/merge.sh
+++ scripts/merge.sh
@@ -4,7 +4,8 @@ source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount
 cd ~/${EOS_nTuples_dir}/
 pwd
 hadd -f Bsphiphi_MC_nocut.root BsphiKK_468_PIDcut.root BsphiKK_469_PIDcut.root &
-hadd -f BsphiKK_MC_nocut.root BsphiKK_470_PIDcut.root BsphiKK_471_PIDcut.root &
+#hadd -f BsphiKK_MC_nocut.root BsphiKK_470_PIDcut.root BsphiKK_471_PIDcut.root & # 470 and 471 should be identical to 573 and 574. Just processed a year apart.
+hadd -f BsphiKK_MC_nocut.root BsphiKK_573_PIDcut.root BsphiKK_574_PIDcut.root BsphiKK_576_PIDcut.root BsphiKK_577_PIDcut.root &
 hadd -f BdphiKst_MC_nocut.root BsphiKK_472_PIDcut.root BsphiKK_473_PIDcut.root &
 hadd -f Bsphipipi_MC_nocut.root BsphiKK_474_PIDcut.root BsphiKK_475_PIDcut.root &
 hadd -f LbphiKp_MC_nocut.root BsphiKK_476_PIDcut.root BsphiKK_477_PIDcut.root &
