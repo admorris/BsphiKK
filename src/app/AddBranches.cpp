49492d7fce44742d0e86ad3ec0cfbd991298a4e9
Adam Morris <adam.morris@cern.ch>
2016-07-07 20:48:23 +0200

    replaced aliased classes with the real ones

--- src/app/AddBranches.cpp
+++ src/app/AddBranches.cpp
@@ -35,63 +35,63 @@ void addBranches(string inputfilename = "BsphiKK_data_cuts.root", string outputf
   outfile->cd();
   TTree* outtree = intree->CloneTree(0);
   // Read the number of events in the input file
-  Int_t n = intree->GetEntries();
+  int n = intree->GetEntries();
 /*PDG masses*******************************************************************/
-  Double_t Kmass = 493.667, pimass = 139.570, pmass = 938.272;
+  double Kmass = 493.667, pimass = 139.570, pmass = 938.272;
 /*Variables for new branches***************************************************/
   // Integers for mass hypothesis decisions (0-3: correspond to track indices)
-  Int_t pion, proton, kaon;
+  int pion, proton, kaon;
   // Track 4-momentum
   TLorentzVector hP[4], h_TRUEP[4], h_BCONP[4], pionP, protonP;
   // B and daughter 4-momentum
   TLorentzVector BP, dP[4];
 /*Input branches***************************************************************/
   // These ones are used in creating the MVA discriminating variables
-  Double_t B_s0_FDCHI2_OWNPV;   intree->SetBranchAddress("B_s0_FDCHI2_OWNPV",  &B_s0_FDCHI2_OWNPV  );
-  Double_t B_s0_IPCHI2_OWNPV;   intree->SetBranchAddress("B_s0_IPCHI2_OWNPV",  &B_s0_IPCHI2_OWNPV  );
-  Double_t B_s0_ENDVERTEX_CHI2; intree->SetBranchAddress("B_s0_ENDVERTEX_CHI2",&B_s0_ENDVERTEX_CHI2);
-  Double_t B_s0_P;              intree->SetBranchAddress("B_s0_P",             &B_s0_P             );
-  Double_t B_s0_PT;             intree->SetBranchAddress("B_s0_PT",            &B_s0_PT            );
+  double B_s0_FDCHI2_OWNPV;   intree->SetBranchAddress("B_s0_FDCHI2_OWNPV",  &B_s0_FDCHI2_OWNPV  );
+  double B_s0_IPCHI2_OWNPV;   intree->SetBranchAddress("B_s0_IPCHI2_OWNPV",  &B_s0_IPCHI2_OWNPV  );
+  double B_s0_ENDVERTEX_CHI2; intree->SetBranchAddress("B_s0_ENDVERTEX_CHI2",&B_s0_ENDVERTEX_CHI2);
+  double B_s0_P;              intree->SetBranchAddress("B_s0_P",             &B_s0_P             );
+  double B_s0_PT;             intree->SetBranchAddress("B_s0_PT",            &B_s0_PT            );
 /*LOKI PX Branches*************************************************************/
-  Double_t h_LOKI_PX[4];
+  double h_LOKI_PX[4];
   intree->SetBranchAddress("B_s0_PX_kaon1",&h_LOKI_PX[0]);
   intree->SetBranchAddress("B_s0_PX_kaon0",&h_LOKI_PX[1]);
   intree->SetBranchAddress("B_s0_PX_kaon3",&h_LOKI_PX[2]);
   intree->SetBranchAddress("B_s0_PX_kaon2",&h_LOKI_PX[3]);
 /*LOKI PY Branches*************************************************************/
-  Double_t h_LOKI_PY[4];
+  double h_LOKI_PY[4];
   intree->SetBranchAddress("B_s0_PY_kaon1",&h_LOKI_PY[0]);
   intree->SetBranchAddress("B_s0_PY_kaon0",&h_LOKI_PY[1]);
   intree->SetBranchAddress("B_s0_PY_kaon3",&h_LOKI_PY[2]);
   intree->SetBranchAddress("B_s0_PY_kaon2",&h_LOKI_PY[3]);
 /*LOKI PZ Branches*************************************************************/
-  Double_t h_LOKI_PZ[4];
+  double h_LOKI_PZ[4];
   intree->SetBranchAddress("B_s0_PZ_kaon1",&h_LOKI_PZ[0]);
   intree->SetBranchAddress("B_s0_PZ_kaon0",&h_LOKI_PZ[1]);
   intree->SetBranchAddress("B_s0_PZ_kaon3",&h_LOKI_PZ[2]);
   intree->SetBranchAddress("B_s0_PZ_kaon2",&h_LOKI_PZ[3]);
 /*BCON PX Branches*************************************************************/
-  Double_t h_BCON_PX[4];
+  double h_BCON_PX[4];
   intree->SetBranchAddress("B_s0_BCON_PX_kaon1",&h_BCON_PX[0]);
   intree->SetBranchAddress("B_s0_BCON_PX_kaon0",&h_BCON_PX[1]);
   intree->SetBranchAddress("B_s0_BCON_PX_kaon3",&h_BCON_PX[2]);
   intree->SetBranchAddress("B_s0_BCON_PX_kaon2",&h_BCON_PX[3]);
 /*BCON PY Branches*************************************************************/
-  Double_t h_BCON_PY[4];
+  double h_BCON_PY[4];
   intree->SetBranchAddress("B_s0_BCON_PY_kaon1",&h_BCON_PY[0]);
   intree->SetBranchAddress("B_s0_BCON_PY_kaon0",&h_BCON_PY[1]);
   intree->SetBranchAddress("B_s0_BCON_PY_kaon3",&h_BCON_PY[2]);
   intree->SetBranchAddress("B_s0_BCON_PY_kaon2",&h_BCON_PY[3]);
 /*BCON PZ Branches*************************************************************/
-  Double_t h_BCON_PZ[4];
+  double h_BCON_PZ[4];
   intree->SetBranchAddress("B_s0_BCON_PZ_kaon1",&h_BCON_PZ[0]);
   intree->SetBranchAddress("B_s0_BCON_PZ_kaon0",&h_BCON_PZ[1]);
   intree->SetBranchAddress("B_s0_BCON_PZ_kaon3",&h_BCON_PZ[2]);
   intree->SetBranchAddress("B_s0_BCON_PZ_kaon2",&h_BCON_PZ[3]);
 /*MC only branches*************************************************************/
-  Double_t h_TRUEPX[4];
-  Double_t h_TRUEPY[4];
-  Double_t h_TRUEPZ[4];
+  double h_TRUEPX[4];
+  double h_TRUEPY[4];
+  double h_TRUEPZ[4];
   if(isMC)
   {
 /*TRUEPX Branches**************************************************************/
@@ -111,89 +111,89 @@ void addBranches(string inputfilename = "BsphiKK_data_cuts.root", string outputf
     intree->SetBranchAddress("Kplus0_TRUEP_Z", &h_TRUEPZ[3]);
   }
 /*ProbNNp Branches*************************************************************/
-  Double_t h_ProbNNp[4];
+  double h_ProbNNp[4];
   intree->SetBranchAddress("Kminus_ProbNNp", &h_ProbNNp[0]);
   intree->SetBranchAddress("Kplus_ProbNNp",  &h_ProbNNp[1]);
   intree->SetBranchAddress("Kminus0_ProbNNp",&h_ProbNNp[2]);
   intree->SetBranchAddress("Kplus0_ProbNNp", &h_ProbNNp[3]);
 /*ProbNNk Branches*************************************************************/
-  Double_t h_ProbNNk[4];
+  double h_ProbNNk[4];
   intree->SetBranchAddress("Kminus_ProbNNk", &h_ProbNNk[0]);
   intree->SetBranchAddress("Kplus_ProbNNk",  &h_ProbNNk[1]);
   intree->SetBranchAddress("Kminus0_ProbNNk",&h_ProbNNk[2]);
   intree->SetBranchAddress("Kplus0_ProbNNk", &h_ProbNNk[3]);
 /*ProbNNpi Branches************************************************************/
-  Double_t h_ProbNNpi[4];
+  double h_ProbNNpi[4];
   intree->SetBranchAddress("Kminus_ProbNNpi", &h_ProbNNpi[0]);
   intree->SetBranchAddress("Kplus_ProbNNpi",  &h_ProbNNpi[1]);
   intree->SetBranchAddress("Kminus0_ProbNNpi",&h_ProbNNpi[2]);
   intree->SetBranchAddress("Kplus0_ProbNNpi", &h_ProbNNpi[3]);
 /*IPCHI2_OWNPV Branches********************************************************/
-  Double_t h_IPCHI2_OWNPV[4];
+  double h_IPCHI2_OWNPV[4];
   intree->SetBranchAddress("Kminus_IPCHI2_OWNPV", &h_IPCHI2_OWNPV[0]);
   intree->SetBranchAddress("Kplus_IPCHI2_OWNPV",  &h_IPCHI2_OWNPV[1]);
   intree->SetBranchAddress("Kminus0_IPCHI2_OWNPV",&h_IPCHI2_OWNPV[2]);
   intree->SetBranchAddress("Kplus0_IPCHI2_OWNPV", &h_IPCHI2_OWNPV[3]);
 /*BDT branches*****************************************************************/
-  Float_t B_s0_ln_FDCHI2; outtree->Branch("B_s0_ln_FDCHI2", &B_s0_ln_FDCHI2, "B_s0_ln_FDCHI2/F" );
-  Float_t B_s0_ln_IPCHI2; outtree->Branch("B_s0_ln_IPCHI2", &B_s0_ln_IPCHI2, "B_s0_ln_IPCHI2/F" );
-  Float_t B_s0_ln_EVCHI2; outtree->Branch("B_s0_ln_EVCHI2", &B_s0_ln_EVCHI2, "B_s0_ln_EVCHI2/F" );
-  Float_t B_s0_PT_fiveGeV;outtree->Branch("B_s0_PT_fiveGeV",&B_s0_PT_fiveGeV,"B_s0_PT_fiveGeV/F");
-  Float_t B_s0_Eta;       outtree->Branch("B_s0_Eta",       &B_s0_Eta,       "B_s0_Eta/F"       );
-  Float_t minK_ln_IPCHI2; outtree->Branch("minK_ln_IPCHI2", &minK_ln_IPCHI2, "minK_ln_IPCHI2/F" );
-  Float_t minK_PT_GeV;    outtree->Branch("minK_PT_GeV",    &minK_PT_GeV,    "minK_PT_GeV/F"    );
-  Double_t h_LOKI_PT[4]; // Necessary for min Kaon PT
+  float B_s0_ln_FDCHI2; outtree->Branch("B_s0_ln_FDCHI2", &B_s0_ln_FDCHI2, "B_s0_ln_FDCHI2/F" );
+  float B_s0_ln_IPCHI2; outtree->Branch("B_s0_ln_IPCHI2", &B_s0_ln_IPCHI2, "B_s0_ln_IPCHI2/F" );
+  float B_s0_ln_EVCHI2; outtree->Branch("B_s0_ln_EVCHI2", &B_s0_ln_EVCHI2, "B_s0_ln_EVCHI2/F" );
+  float B_s0_PT_fiveGeV;outtree->Branch("B_s0_PT_fiveGeV",&B_s0_PT_fiveGeV,"B_s0_PT_fiveGeV/F");
+  float B_s0_Eta;       outtree->Branch("B_s0_Eta",       &B_s0_Eta,       "B_s0_Eta/F"       );
+  float minK_ln_IPCHI2; outtree->Branch("minK_ln_IPCHI2", &minK_ln_IPCHI2, "minK_ln_IPCHI2/F" );
+  float minK_PT_GeV;    outtree->Branch("minK_PT_GeV",    &minK_PT_GeV,    "minK_PT_GeV/F"    );
+  double h_LOKI_PT[4]; // Necessary for min Kaon PT
 /*New mass branches************************************************************/
   // phi KK
-  TLorentzVector BCON_KK_P;   Double_t BCON_KK_M;         outtree->Branch("BCON_KK_M",  &BCON_KK_M,  "BCON_KK_M/D"  );
-  TLorentzVector KK_TRUEP;    Double_t KK_TRUEM; if(isMC) outtree->Branch("KK_TRUEM",   &KK_TRUEM,   "KK_TRUEM/D"   );
-  TLorentzVector phiKplusP;   Double_t phiKplusM;         outtree->Branch("phiKplusM",  &phiKplusM,  "phiKplusM/D"  );
-  TLorentzVector phiKminusP;  Double_t phiKminusM;        outtree->Branch("phiKminusM", &phiKminusM, "phiKminusM/D" );
+  TLorentzVector BCON_KK_P;   double BCON_KK_M;         outtree->Branch("BCON_KK_M",  &BCON_KK_M,  "BCON_KK_M/D"  );
+  TLorentzVector KK_TRUEP;    double KK_TRUEM; if(isMC) outtree->Branch("KK_TRUEM",   &KK_TRUEM,   "KK_TRUEM/D"   );
+  TLorentzVector phiKplusP;   double phiKplusM;         outtree->Branch("phiKplusM",  &phiKplusM,  "phiKplusM/D"  );
+  TLorentzVector phiKminusP;  double phiKminusM;        outtree->Branch("phiKminusM", &phiKminusM, "phiKminusM/D" );
   // phi pipi
-  TLorentzVector phipipiP;    Double_t phipipiM;          outtree->Branch("phipipiM",   &phipipiM,   "phipipiM/D"   );
-  TLorentzVector pipiP;       Double_t pipiM;             outtree->Branch("pipiM",      &pipiM,      "pipiM/D"      );
-  TLorentzVector phipiplusP;  Double_t phipiplusM;        outtree->Branch("phipiplusM", &phipiplusM, "phipiplusM/D" );
-  TLorentzVector phipiminusP; Double_t phipiminusM;       outtree->Branch("phipiminusM",&phipiminusM,"phipiminusM/D");
+  TLorentzVector phipipiP;    double phipipiM;          outtree->Branch("phipipiM",   &phipipiM,   "phipipiM/D"   );
+  TLorentzVector pipiP;       double pipiM;             outtree->Branch("pipiM",      &pipiM,      "pipiM/D"      );
+  TLorentzVector phipiplusP;  double phipiplusM;        outtree->Branch("phipiplusM", &phipiplusM, "phipiplusM/D" );
+  TLorentzVector phipiminusP; double phipiminusM;       outtree->Branch("phipiminusM",&phipiminusM,"phipiminusM/D");
   // phi K pi
-  TLorentzVector KpiP;        Double_t KpiM;              outtree->Branch("KpiM",       &KpiM,       "KpiM/D"       );
-  TLorentzVector phiKpiP;     Double_t phiKpiM;           outtree->Branch("phiKpiM",    &phiKpiM,    "phiKpiM/D"    );
+  TLorentzVector KpiP;        double KpiM;              outtree->Branch("KpiM",       &KpiM,       "KpiM/D"       );
+  TLorentzVector phiKpiP;     double phiKpiM;           outtree->Branch("phiKpiM",    &phiKpiM,    "phiKpiM/D"    );
   // phi K p
-  TLorentzVector phipP;       Double_t phipM;             outtree->Branch("phipM",      &phipM,      "phipM/D"      );
-  TLorentzVector phipbarP;    Double_t phipbarM;          outtree->Branch("phipbarM",   &phipbarM,   "phipbarM/D"   );
-  TLorentzVector KpP;         Double_t KpM;               outtree->Branch("KpM",        &KpM,        "KpM/D"        );
-  TLorentzVector phiKpP;      Double_t phiKpM;            outtree->Branch("phiKpM",     &phiKpM,     "phiKpM/D"     );
+  TLorentzVector phipP;       double phipM;             outtree->Branch("phipM",      &phipM,      "phipM/D"      );
+  TLorentzVector phipbarP;    double phipbarM;          outtree->Branch("phipbarM",   &phipbarM,   "phipbarM/D"   );
+  TLorentzVector KpP;         double KpM;               outtree->Branch("KpM",        &KpM,        "KpM/D"        );
+  TLorentzVector phiKpP;      double phiKpM;            outtree->Branch("phiKpM",     &phiKpM,     "phiKpM/D"     );
   // phi pi p
-  TLorentzVector pipP;        Double_t pipM;              outtree->Branch("pipM",       &pipM,       "pipM/D"       );
-  TLorentzVector pipbarP;     Double_t pipbarM;           outtree->Branch("pipbarM",    &pipbarM,    "pibarM/D"     );
-  TLorentzVector phipipP;     Double_t phipipM;           outtree->Branch("phipipM",    &phipipM,    "phipipM/D"    );
-  TLorentzVector phipipbarP;  Double_t phipipbarM;        outtree->Branch("phipipbarM", &phipipbarM, "phipipbarM/D" );
+  TLorentzVector pipP;        double pipM;              outtree->Branch("pipM",       &pipM,       "pipM/D"       );
+  TLorentzVector pipbarP;     double pipbarM;           outtree->Branch("pipbarM",    &pipbarM,    "pibarM/D"     );
+  TLorentzVector phipipP;     double phipipM;           outtree->Branch("phipipM",    &phipipM,    "phipipM/D"    );
+  TLorentzVector phipipbarP;  double phipipbarM;        outtree->Branch("phipipbarM", &phipipbarM, "phipipbarM/D" );
   // PID variables for tracks identified as something else
-  Double_t pion_ProbNNk;    outtree->Branch("pion_ProbNNk",   &pion_ProbNNk,   "pion_ProbNNk/D" );
-  Double_t pion_ProbNNpi;   outtree->Branch("pion_ProbNNpi",  &pion_ProbNNpi,  "pion_ProbNNpi/D");
-  Double_t pion_ProbNNp;    outtree->Branch("pion_ProbNNp",   &pion_ProbNNp,   "pion_ProbNNp/D" );
-  Double_t proton_ProbNNk;  outtree->Branch("proton_ProbNNk", &proton_ProbNNk, "proton_ProbNNk/D" );
-  Double_t proton_ProbNNpi; outtree->Branch("proton_ProbNNpi",&proton_ProbNNpi,"proton_ProbNNpi/D");
-  Double_t proton_ProbNNp;  outtree->Branch("proton_ProbNNp", &proton_ProbNNp, "proton_ProbNNp/D" );
+  double pion_ProbNNk;    outtree->Branch("pion_ProbNNk",   &pion_ProbNNk,   "pion_ProbNNk/D" );
+  double pion_ProbNNpi;   outtree->Branch("pion_ProbNNpi",  &pion_ProbNNpi,  "pion_ProbNNpi/D");
+  double pion_ProbNNp;    outtree->Branch("pion_ProbNNp",   &pion_ProbNNp,   "pion_ProbNNp/D" );
+  double proton_ProbNNk;  outtree->Branch("proton_ProbNNk", &proton_ProbNNk, "proton_ProbNNk/D" );
+  double proton_ProbNNpi; outtree->Branch("proton_ProbNNpi",&proton_ProbNNpi,"proton_ProbNNpi/D");
+  double proton_ProbNNp;  outtree->Branch("proton_ProbNNp", &proton_ProbNNp, "proton_ProbNNp/D" );
 /*Helicity angle branches******************************************************/
   // Track 4-momentum in B frame and daughter frames
   TLorentzVector Bframe_h_P[4], /*Bframe_d_P[4],*/ dframe_h_P[4], dframe_other_h_P[4];
   TVector3 Bframe_e, dframe_e[2], dframe_n[2];
 //  TVector3 CrossProduct[2];
-  Double_t Phi_angle; outtree->Branch("Phi_angle", &Phi_angle, "Phi_angle/D" );
-  Double_t cos_Phi  ; outtree->Branch("cos_Phi"  , &cos_Phi  , "cos_Phi/D"   );
-  Double_t sin_Phi  ; outtree->Branch("sin_Phi"  , &sin_Phi  , "sin_Phi/D"   );
-  Double_t cos_theta[2];
+  double Phi_angle; outtree->Branch("Phi_angle", &Phi_angle, "Phi_angle/D" );
+  double cos_Phi  ; outtree->Branch("cos_Phi"  , &cos_Phi  , "cos_Phi/D"   );
+  double sin_Phi  ; outtree->Branch("sin_Phi"  , &sin_Phi  , "sin_Phi/D"   );
+  double cos_theta[2];
   outtree->Branch("cos_theta1",&cos_theta[0],"cos_theta1/D");
   outtree->Branch("cos_theta2",&cos_theta[1],"cos_theta2/D");
   // Same branches, but with constrained B mass.
-  Double_t BCON_Phi_angle; outtree->Branch("BCON_Phi_angle", &BCON_Phi_angle, "BCON_Phi_angle/D" );
-  Double_t BCON_cos_Phi  ; outtree->Branch("BCON_cos_Phi"  , &BCON_cos_Phi  , "BCON_cos_Phi/D"   );
-  Double_t BCON_sin_Phi  ; outtree->Branch("BCON_sin_Phi"  , &BCON_sin_Phi  , "BCON_sin_Phi/D"   );
-  Double_t BCON_cos_theta[2];
+  double BCON_Phi_angle; outtree->Branch("BCON_Phi_angle", &BCON_Phi_angle, "BCON_Phi_angle/D" );
+  double BCON_cos_Phi  ; outtree->Branch("BCON_cos_Phi"  , &BCON_cos_Phi  , "BCON_cos_Phi/D"   );
+  double BCON_sin_Phi  ; outtree->Branch("BCON_sin_Phi"  , &BCON_sin_Phi  , "BCON_sin_Phi/D"   );
+  double BCON_cos_theta[2];
   outtree->Branch("BCON_cos_theta1",&BCON_cos_theta[0],"BCON_cos_theta1/D");
   outtree->Branch("BCON_cos_theta2",&BCON_cos_theta[1],"BCON_cos_theta2/D");
 /*Event loop*******************************************************************/
-  for(Int_t i = 0; i < n; i++)
+  for(int i = 0; i < n; i++)
   {
     intree->GetEntry(i);
 /*BDT variables****************************************************************/
@@ -203,22 +203,22 @@ void addBranches(string inputfilename = "BsphiKK_data_cuts.root", string outputf
     B_s0_PT_fiveGeV = B_s0_PT/5000; // Change units for sensible range of numbers
     B_s0_Eta        = TMath::ACosH(B_s0_P/B_s0_PT);
     minK_ln_IPCHI2  = safeLog(minOfFour(h_IPCHI2_OWNPV[0],h_IPCHI2_OWNPV[1],h_IPCHI2_OWNPV[2],h_IPCHI2_OWNPV[3]));
-    for(Int_t j = 0; j < 4; j++) h_LOKI_PT[j] = TMath::Sqrt(TMath::Power(h_LOKI_PX[j],2)+TMath::Power(h_LOKI_PY[j],2));
+    for(int j = 0; j < 4; j++) h_LOKI_PT[j] = TMath::Sqrt(TMath::Power(h_LOKI_PX[j],2)+TMath::Power(h_LOKI_PY[j],2));
     minK_PT_GeV     = minOfFour(h_LOKI_PT[0],h_LOKI_PT[1],h_LOKI_PT[2],h_LOKI_PT[3])/1000;
 /*Mass branches****************************************************************/
     // MC truth mass
     if(isMC)
     {
-      for(Int_t j = 0; j < 4; j++) h_TRUEP[j].SetXYZM(h_TRUEPX[j],h_TRUEPY[j],h_TRUEPZ[j],Kmass);
+      for(int j = 0; j < 4; j++) h_TRUEP[j].SetXYZM(h_TRUEPX[j],h_TRUEPY[j],h_TRUEPZ[j],Kmass);
       KK_TRUEP = h_TRUEP[2] + h_TRUEP[3];
       KK_TRUEM = KK_TRUEP.M();
     }
     // Track 4-momenta with constrained Bs mass
-    for(Int_t j = 0; j < 4; j++) h_BCONP[j].SetXYZM(h_BCON_PX[j],h_BCON_PY[j],h_BCON_PZ[j],Kmass);
+    for(int j = 0; j < 4; j++) h_BCONP[j].SetXYZM(h_BCON_PX[j],h_BCON_PY[j],h_BCON_PZ[j],Kmass);
     BCON_KK_P = h_BCONP[2] + h_BCONP[3];
     BCON_KK_M = BCON_KK_P.M();
     // Initial 4K hypothesis
-    for(Int_t j = 0; j < 4; j++) hP[j].SetXYZM(h_LOKI_PX[j],h_LOKI_PY[j],h_LOKI_PZ[j],Kmass);
+    for(int j = 0; j < 4; j++) hP[j].SetXYZM(h_LOKI_PX[j],h_LOKI_PY[j],h_LOKI_PZ[j],Kmass);
     // Reconstruct B and daughter 4-momentum
     BP    = hP[0] + hP[1] + hP[2] + hP[3];
     dP[0] = hP[0] + hP[1];
@@ -328,7 +328,7 @@ void addBranches(string inputfilename = "BsphiKK_data_cuts.root", string outputf
         dP[1] = h_BCONP[2] + h_BCONP[3];
       }
       // Loop over Kaons
-      for(Int_t j = 0; j < 4; j++)
+      for(int j = 0; j < 4; j++)
       {
         // Boost into B frame
         Bframe_h_P[j] = BCON==1 ? h_BCONP[j] : hP[j];
@@ -342,10 +342,10 @@ void addBranches(string inputfilename = "BsphiKK_data_cuts.root", string outputf
         dframe_other_h_P[j].Boost(-dP[(j-3)/-2].BoostVector());
       }
       // Loop over daughters
-      for(Int_t j = 0; j < 2; j++)
+      for(int j = 0; j < 2; j++)
       {
-        Int_t minus = 2*j;   // 0 and 2
-        Int_t plus  = 2*j+1; // 1 and 3
+        int minus = 2*j;   // 0 and 2
+        int plus  = 2*j+1; // 1 and 3
         dframe_e[j] = -1.0 * ((dframe_other_h_P[minus].Vect() + dframe_other_h_P[plus].Vect()) * (1.0/(dframe_other_h_P[minus].Vect() + dframe_other_h_P[plus].Vect()).Mag()));
         if(BCON == 0)
         {
