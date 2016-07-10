31a69e8585c83f1c7fe04d7ef2d919f5f5a23497
Adam Morris <adam.morris@cern.ch>
2016-07-10 17:34:52 +0200

    scripts

---
+++ scripts/angacc_hist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ..
+bin/AngularAcceptance_AdaptiveHist ntuples/BsphiKK_MC_mvacut.root ntuples/BsphiKK_Gen_mvaVars.root
+bin/AddBranches_Acceptance Acceptance.root ntuples/Bsphiphi_Gen_mvaVars.root Bsphiphi_Gen_mvaVars_acc_weights.root
+
---
+++ scripts/plotIPchi2branches.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+cd ../ntuples
+#../bin/PlotBranch \
+#    -F BsphiKK_MC_nocut_mvaVars.root \
+#    -B Kplus0_IPCHI2_OWNPV \
+#    -T "#it{K}^{#plus} #it{#chi}^{2}_{IP}" \
+#    -O ../Kplus0_IPCHI2_OWNPV \
+#    -l 0 \
+#    -u 50 \
+#    -b 50
+#../bin/PlotBranch \
+#    -F BsphiKK_MC_nocut_mvaVars.root \
+#    -B Kminus0_IPCHI2_OWNPV \
+#    -T "#it{K}^{#minus} #it{#chi}^{2}_{IP}" \
+#    -O ../Kminus0_IPCHI2_OWNPV \
+#    -l 0 \
+#    -u 50 \
+#    -b 50
+#../bin/PlotBranch \
+#    -F BsphiKK_MC_nocut_mvaVars.root \
+#    -B Kplus_IPCHI2_OWNPV \
+#    -T "#it{K}^{#plus} #it{#chi}^{2}_{IP}" \
+#    -O ../Kplus_IPCHI2_OWNPV \
+#    -l 0 \
+#    -u 50 \
+#    -b 50
+#../bin/PlotBranch \
+#    -F BsphiKK_MC_nocut_mvaVars.root \
+#    -B Kminus_IPCHI2_OWNPV \
+#    -T "#it{K}^{#minus} #it{#chi}^{2}_{IP}" \
+#    -O ../Kminus_IPCHI2_OWNPV \
+#    -l 0 \
+#    -u 50 \
+#    -b 50
+../bin/PlotBranch \
+    -F BsphiKK_MC_nocut_mvaVars.root \
+    -B KK_IPCHI2_OWNPV \
+    -T "#it{K}^{#plus}#it{K}^{#minus} #it{#chi}^{2}_{IP}" \
+    -O ../KK_IPCHI2_OWNPV \
+    -l 0 \
+    -u 50 \
+    -b 50
+../bin/PlotBranch \
+    -F BsphiKK_MC_nocut_mvaVars.root \
+    -B phi_1020_IPCHI2_OWNPV \
+    -T "#it{#phi} #it{#chi}^{2}_{IP}" \
+    -O ../phi_1020_IPCHI2_OWNPV \
+    -l 0 \
+    -u 50 \
+    -b 50
+../bin/PlotBranch \
+    -F BsphiKK_MC_nocut_mvaVars.root \
+    -B B_s0_IPCHI2_OWNPV \
+    -T "#it{B}^{0}_{#it{s}} #it{#chi}^{2}_{IP}" \
+    -O ../B_s0_IPCHI2_OWNPV \
+    -l 0 \
+    -u 50 \
+    -b 50
