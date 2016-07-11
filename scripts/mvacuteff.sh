dd0c51adcca69c9556e9bfb7794f8951a89c74db
Adam Morris <adam.morris@cern.ch>
2016-07-11 22:31:15 +0200

    automatic mva cut eff

--- scripts/mvacuteff.sh
+++ scripts/mvacuteff.sh
@@ -9,4 +9,6 @@ cd ../ntuples/
                  -C "TMath::Abs(KK_M-1019.461)<15" \
                  -O ../latex/figs/Bsphiphi_fit_mvacut \
                  --pulls
-
+../bin/GetCutEff ../ntuples/Bsphiphi_MC_mva.root "mlp>0.62" --save --output-file ../scripts/tables/mvaeffs.csv --result-name BsphiphiMCmvacuteff
+../bin/GetCutEff ../ntuples/BsphiKK_MC_mva.root  "mlp>0.62" --save --output-file ../scripts/tables/mvaeffs.csv --result-name BsphiKKMCmvacuteff
+../bin/ExportResults ../scripts/tables/mvaeffs.csv ../latex/results/mvaeffs.tex
