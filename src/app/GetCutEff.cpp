bdea29fd2db8a1294e2d6395d25ee91c8959aee6
Adam Morris <adam.morris@cern.ch>
2016-07-11 22:18:24 +0200

    app for saving single cut results to a file

---
+++ src/app/GetCutEff.cpp
@@ -0,0 +1,57 @@
+// C++ headers
+#include <string>
+#include <iostream>
+#include <iomanip>
+// BOOST headers
+#include "boost/program_options.hpp"
+// Custom headers
+#include "CutEff.h"
+#include "ResultDB.h"
+#include "GetTree.h"
+using namespace std;
+void GetCutEff(string filename, string beforecut, string aftercut, bool save, string resname, string DBfilename)
+{
+  TTree* intree = GetTree(filename);
+  new TCanvas;
+  CutResult_t result = CutEff(filename, beforecut, aftercut);
+  if(save)
+  {
+    ResultDB rdb(DBfilename);
+    rdb.Update(resname,"percent",result.GetEff(),result.GetEffErr());
+    rdb.Save();
+  }
+}
+int main(int argc, char* argv[])
+{
+  string filename, beforecut, aftercut, dbf, res;
+  using namespace boost::program_options;
+  options_description desc("Allowed options",120);
+  desc.add_options()
+    ("help,H",                                                            "produce help message")
+    ("save"  ,                                                            "save the results"    )
+    ("output-file", value<string>(&dbf)->default_value("CutEff.csv"    ), "output file"         )
+    ("result-name", value<string>(&res)->default_value("efficiency"    ), "result name"         )
+    ("input-file" , value<string>(&filename                            ), "input file"          )
+    ("input-file" , value<string>(&beforecut)->default_value("B_s0_M>0"), "initial cut"         )
+    ("final-cut"  , value<string>(&aftercut                            ), "final cut"           )
+  ;
+  variables_map vmap;
+  positional_options_description pd;
+  pd.add("input-file", 1);
+  pd.add("initialcut", 2);
+  pd.add("finalcut"  , 3);
+  store(command_line_parser(argc, argv).options(desc).positional(pd).run(), vmap);
+  notify(vmap);
+  if (vmap.count("help"))
+  {
+    std::cout << desc << endl;
+    return 1;
+  }
+  if(filename.empty())
+  {
+    cout << "Usage: " << argv[0] << " <filename>" << endl;
+    return 1;
+  }
+  GetCutEff(filename,beforecut,aftercut,vmap.count("save"),res,dbf);
+  return 0;
+}
