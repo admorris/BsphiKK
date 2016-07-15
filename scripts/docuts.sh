1adc7f52d90961173d738ab270e73f780cb61eaa
Adam Morris <adam.morris@cern.ch>
2016-07-15 14:28:51 +0200

    don't try to concurrently access the same file

--- scripts/docuts.sh
+++ scripts/docuts.sh
@@ -5,20 +5,18 @@ cd ../ntuples/
 #Sum of all cuts
 totalcut="${trigcut}&&${ghstcut}&&${trackisMuoncut}&&${phiMcut}&&${KpTcut}&&${phiIPCHI2cut}&&${KKIPCHI2cut}&&${BsFDCHI2cut}&&${BsIPCHI2cut}&&${KpiPIDcut}&&${KpPIDcut}"
 ###########################################################
-modes=(BsphiKK_data BsphiKK_sideband Bsphiphi_MC BsphiKK_MC BdphiKst_MC Bsphipipi_MC LbphiKp_MC)
+modes=(BsphiKK_data Bsphiphi_MC BsphiKK_MC BdphiKst_MC Bsphipipi_MC LbphiKp_MC)
 for mode in ${modes[@]}
 do
   if [ "$mode" == "Bsphiphi_MC"  -o  "$mode" == "BsphiKK_MC" ]
   then
     cutapplier ${EOS_server}${EOS_nTuples_dir}${mode}_nocut.root DecayTreeTuple/DecayTree "${totalcut}&&${BKGCATcut}&&${BsMcut}" ${mode}_duplicates.root &
-  elif [ "$mode" == "BsphiKK_sideband" ]
-  then
-    cutapplier ${EOS_server}${EOS_nTuples_dir}BsphiKK_data_nocut.root DecayTreeTuple/DecayTree "${totalcut}&&B_s0_LOKI_Mass>5500" ${mode}_duplicates.root &
   else
     cutapplier ${EOS_server}${EOS_nTuples_dir}${mode}_nocut.root DecayTreeTuple/DecayTree "${totalcut}&&${BsMcut}" ${mode}_duplicates.root &
   fi
 done
 wait
+cutapplier ${EOS_server}${EOS_nTuples_dir}BsphiKK_data_nocut.root DecayTreeTuple/DecayTree "${totalcut}&&B_s0_LOKI_Mass>5500" BsphiKK_sideband_duplicates.root
 for mode in ${modes[@]}
 do
   ../bin/FlagClones ${mode}_duplicates.root DecayTreeTuple/DecayTree
