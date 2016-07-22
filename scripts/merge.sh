a772b01021f1832ccc549bbc94f39a4c32263a6c
Adam Morris <adam.morris@cern.ch>
2016-07-22 17:26:35 +0200

    nocut and loosePID better names

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -5,13 +5,18 @@ source /afs/cern.ch/project/eos/installation/lhcb/etc/setup.sh
 source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount ~/eos
 cd ~/${EOS_nTuples_dir}/
 pwd
-hadd -f Bsphiphi_MC_nocut.root BsphiKK_468_PIDcut.root BsphiKK_469_PIDcut.root &
-#hadd -f BsphiKK_MC_nocut.root BsphiKK_470_PIDcut.root BsphiKK_471_PIDcut.root & # 470 and 471 should be identical to 573 and 574. Just processed a year apart.
-hadd -f BsphiKK_MC_nocut.root BsphiKK_573_PIDcut.root BsphiKK_574_PIDcut.root BsphiKK_576_PIDcut.root BsphiKK_577_PIDcut.root &
-hadd -f BdphiKst_MC_nocut.root BsphiKK_472_PIDcut.root BsphiKK_473_PIDcut.root &
-hadd -f Bsphipipi_MC_nocut.root BsphiKK_474_PIDcut.root BsphiKK_475_PIDcut.root &
-hadd -f LbphiKp_MC_nocut.root BsphiKK_476_PIDcut.root BsphiKK_477_PIDcut.root &
-hadd -f BsphiKK_data_nocut.root BsphiKK_478_PIDcut.root BsphiKK_479_PIDcut.root BsphiKK_480_PIDcut.root BsphiKK_481_PIDcut.root BsphiKK_483_PIDcut.root BsphiKK_484_PIDcut.root &
+hadd -f Bsphiphi_MC_loosePID.root BsphiKK_468_PIDcut.root BsphiKK_469_PIDcut.root &
+hadd -f BsphiKK_MC_loosePID.root BsphiKK_573_PIDcut.root BsphiKK_574_PIDcut.root BsphiKK_576_PIDcut.root BsphiKK_577_PIDcut.root &
+hadd -f BdphiKst_MC_loosePID.root BsphiKK_472_PIDcut.root BsphiKK_473_PIDcut.root &
+hadd -f Bsphipipi_MC_loosePID.root BsphiKK_474_PIDcut.root BsphiKK_475_PIDcut.root &
+hadd -f LbphiKp_MC_loosePID.root BsphiKK_476_PIDcut.root BsphiKK_477_PIDcut.root &
+hadd -f BsphiKK_data_loosePID.root BsphiKK_478_PIDcut.root BsphiKK_479_PIDcut.root BsphiKK_480_PIDcut.root BsphiKK_481_PIDcut.root BsphiKK_483_PIDcut.root BsphiKK_484_PIDcut.root &
+hadd -f Bsphiphi_MC_nocut.root BsphiKK_468_noPID.root BsphiKK_469_noPID.root &
+hadd -f BsphiKK_MC_nocut.root BsphiKK_573_noPID.root BsphiKK_574_noPID.root BsphiKK_576_noPID.root BsphiKK_577_noPID.root &
+hadd -f BdphiKst_MC_nocut.root BsphiKK_472_noPID.root BsphiKK_473_noPID.root &
+hadd -f Bsphipipi_MC_nocut.root BsphiKK_474_noPID.root BsphiKK_475_noPID.root &
+hadd -f LbphiKp_MC_nocut.root BsphiKK_476_noPID.root BsphiKK_477_noPID.root &
+hadd -f BsphiKK_data_nocut.root BsphiKK_478_noPID.root BsphiKK_479_noPID.root BsphiKK_480_noPID.root BsphiKK_481_noPID.root BsphiKK_483_noPID.root BsphiKK_484_noPID.root &
 wait
 cd -
 source /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse umount ~/eos
