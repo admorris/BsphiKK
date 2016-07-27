a6111bc24d57493eebb69b875b46f81a0c45596d
Adam Morris <adam.morris@cern.ch>
2016-07-27 20:04:15 +0200

    nicer

--- src/app/CompareBranch.cpp
+++ src/app/CompareBranch.cpp
@@ -13,8 +13,7 @@
 // Custom headers
 #include "MakeBranchPlot.h"
 #include "plotmaker.h"
-#include "GetTree.h"
-
+using std::string;
 void CompareBranch(string MCfilename, string REfilename, string branchname, string xtitle, string unit, string plotname, string cuts, string MCweight, string REweight, double xlow, double xup, int nbins)
 {
   // Open the files and get the trees
@@ -43,19 +42,19 @@ int main(int argc, char* argv[])
   double xlow, xup;
   int nbins;
   desc.add_options()
-    ("help,H"    ,                                                                                        "produce help message"                      )
-    ("MCfile,M"  , value<std::string>(&MCfile  )->default_value("ntuples/BsphiKK_MC_mva.root"          ), "set Monte Carlo file"                      )
-    ("REfile,R"  , value<std::string>(&REfile  )->default_value("ntuples/BsphiKK_data_mva.root"        ), "set collision data file"                   )
-    ("branch,B"  , value<std::string>(&branch  )->default_value("B_s0_LOKI_Mass"                       ), "set branch to plot"                        )
-    ("MCweight,w", value<std::string>(&MCweight)->default_value(""                                     ), "set Monte Carlo weighting variable"        )
-    ("REweight,W", value<std::string>(&REweight)->default_value(""                                     ), "set collision data weighting variable"     )
-    ("cuts,C"    , value<std::string>(&cuts    )->default_value(""                                     ), "set optional cuts (UNIMPLEMENTED)"         )
-    ("title,T"   , value<std::string>(&xtitle  )->default_value("#it{m}(#it{#phi K^{#plus}K^{#minus}})"), "set x-axis title (takes ROOT LaTeX format)")
-    ("unit,U"    , value<std::string>(&unit    )->default_value("MeV/#it{c}^{2}"                       ), "set unit (takes ROOT LaTeX format)"        )
-    ("plot,O"    , value<std::string>(&plot    )->default_value("comparison"                           ), "set output plot filename"                  )
-    ("upper,u"   , value<double     >(&xup     )->default_value(5600                                   ), "set branch upper limit"                    )
-    ("lower,l"   , value<double     >(&xlow    )->default_value(5200                                   ), "set branch lower limit"                    )
-    ("bins,b"    , value<int        >(&nbins   )->default_value(20                                     ), "set number of bins"                        )
+    ("help,H"    ,                                                                                   "produce help message"             )
+    ("MCfile,M"  , value<string>(&MCfile  )->default_value("ntuples/BsphiKK_MC_mva.root"          ), "Monte Carlo file"                 )
+    ("REfile,R"  , value<string>(&REfile  )->default_value("ntuples/BsphiKK_data_mva.root"        ), "collision data file"              )
+    ("branch,B"  , value<string>(&branch  )->default_value("B_s0_LOKI_Mass"                       ), "branch to plot"                   )
+    ("MCweight,w", value<string>(&MCweight)->default_value(""                                     ), "Monte Carlo weighting variable"   )
+    ("REweight,W", value<string>(&REweight)->default_value(""                                     ), "collision data weighting variable")
+    ("cuts,C"    , value<string>(&cuts    )->default_value(""                                     ), "optional cuts"                    )
+    ("title,T"   , value<string>(&xtitle  )->default_value("#it{m}(#it{#phi K^{#plus}K^{#minus}})"), "x-axis title"                     )
+    ("unit,U"    , value<string>(&unit    )->default_value("MeV/#it{c}^{2}"                       ), "unit"                             )
+    ("plot,O"    , value<string>(&plot    )->default_value("comparison"                           ), "output plot filename"             )
+    ("upper,u"   , value<double>(&xup     )->default_value(5600                                   ), "branch upper limit"               )
+    ("lower,l"   , value<double>(&xlow    )->default_value(5200                                   ), "branch lower limit"               )
+    ("bins,b"    , value<int   >(&nbins   )->default_value(20                                     ), "number of bins"                   )
   ;
   variables_map vmap;
   store(parse_command_line(argc, argv, desc), vmap);
