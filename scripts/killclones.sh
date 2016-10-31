32f60a8287f19bdbc6db9df3141508ccef8eb808
Adam Morris <adam.morris@cern.ch>
2016-10-31 12:13:49 +0100

    update clone killing

--- scripts/killclones.sh
+++ scripts/killclones.sh
@@ -2,13 +2,13 @@
 cd ../ntuples
 function killclones()
 {
-  ../bin/FlagClones $1.root DecayTree
+  ../bin/FlagClones $1.root
   cutapplier $1_Clone.root DecayTree "isDup==1" $1_final.root
   rm -v $1_Clone.root
 }
 for file in $(ls *mvacut.root | sed 's/\.root//g')
 do
-  killclones ${file} &
+  killclones ${file} | tee log/killclones_$file.log &
 done
 wait
 exit 0
--- src/app/FlagClones.cpp
+++ src/app/FlagClones.cpp
@@ -1,25 +1,20 @@
 #include <string>
 #include <iostream>
-#include "TFile.h"
-#include "TTree.h"
 #include "TBranch.h"
-#include "TChain.h"
 #include "TMath.h"
-#include "TLorentzVector.h"
-#include "TVector2.h"
 #include <vector>
-#include <algorithm>
+#include <stdexcept>
 #include "TMath.h"
 #include "CloneInfo.h"
 #include "CloneTagger.h"
+#include "GetTree.h"
 using namespace std;
-void FlagClones(string fileName = "BsphiKK_data_duplicates.root" , string treeName = "DecayTreeTuple/DecayTree")
+void FlagClones(string fileName = "BsphiKK_data_duplicates.root")
 {
   // get the input
-  TChain* tree = new TChain(treeName.c_str());
-  tree->Add(fileName.c_str());
+  TTree* tree = GetTree(fileName.c_str());
   // clone the tree..
-  int num_entries  = tree->GetEntries();
+  int num_entries = tree->GetEntries();
   vector<int> flag_clones; flag_clones.resize(num_entries);
   for (int i = 0; i < num_entries; ++i)
   {
@@ -32,48 +27,47 @@ void FlagClones(string fileName = "BsphiKK_data_duplicates.root" , string treeNa
   int key2; tree->SetBranchAddress("Kminus0_TRACK_Key",&key2);
   int key3; tree->SetBranchAddress("Kplus_TRACK_Key",&key3);
   int key4; tree->SetBranchAddress("Kplus0_TRACK_Key",&key4);
-  vector<CloneInfo> clones;
-  CloneTagger tagger(clones);
   int i = 0;
-  cout << "Finding duplicate events" << endl;
-  while (i < num_entries)
+  cout << "Finding duplicate candidates in a sample containing " << num_entries << " events" << endl;
+  while(i < num_entries)
   {
     tree->GetEntry(i);
     int run_i = run;
     int event_i = event;
-    clones.clear();
-    tagger.addToClones(key1,key2,key3,key4,i);
+    vector<CloneInfo> clones;
+    CloneTagger tagger(clones);
+    tagger.addToClones({key1,key2,key3,key4},i,dvchi2);
     int j = i+1;
     for (j=i+1;j<num_entries; j++)
     {
       tree->GetEntry(j);
       int run_j = run;
       int event_j = event;
-        if(run_j!=run_i||event_j!=event_i)
-        {
-          break;
-        }
-        else
-        {
-          tagger.addToClones(key1,key2,key3,key4,j);
-        }
+      if(run_j!=run_i || event_j!=event_i)
+      {
+        break;
+      }
+      else
+      {
+        tagger.addToClones({key1,key2,key3,key4},j,dvchi2);
+      }
     }
     tagger.sortClones();
     tagger.tagClones();
-    if (clones.size() > 1)
+    if(clones.size() > 1)
     {
-      for (unsigned int ii = 0; ii < clones.size() ; ++ii)
+      for(auto clone : clones)
       {
-        flag_clones[clones[ii].i()] = clones[ii].isAlive();
+        flag_clones[clone.i()] = clone.isAlive();
       }
     }
-    i = j;
+    i=j;
   }
   // make the output
   string outputName = fileName.substr(0,fileName.size() - 5);
   outputName += "_Clone.root";
-  TFile* outFile  =TFile::Open(outputName.c_str(),"RECREATE");
-  cout << "Reading: " << treeName << " from " << fileName  << " to " << outputName << endl;
+  TFile* outFile = TFile::Open(outputName.c_str(),"RECREATE");
+  cout << "Reading: " << tree->GetName() << " from " << fileName  << " to " << outputName << endl;
   TTree*  newtree = tree->CloneTree(-1);
   int isAlive;
   TBranch *branch_bestVtxChi2 = newtree->Branch("isDup",&isAlive, "isDup/I");
@@ -91,11 +85,11 @@ void FlagClones(string fileName = "BsphiKK_data_duplicates.root" , string treeNa
 }
 int main(int argc, char* argv[])
 {
-  if(argc!=3)
+  if(argc!=2)
   {
-    cout << "Usage: " << argv[0] << " <file name> <tree path/tree name>" << endl;
+    cout << "Usage: " << argv[0] << " <file name>" << endl;
     return 1;
   }
-  FlagClones((string)argv[1], (string)argv[2]);
+  FlagClones((string)argv[1]);
   return 0;
 }
