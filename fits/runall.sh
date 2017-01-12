768e7dd15263f9108da4c3d15bb023f4f50b9cb5
Adam Morris <adam.morris@cern.ch>
2017-01-12 23:09:13 +0000

    run all the available fits

---
+++ fits/runall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+make -C modules -j
+currentdir=$(pwd)
+rapidfit=/scratch/adam/RapidFit/bin/fitting
+fitfolders=(toystudies mcfits/phasespace mcfits/pwave datafits)
+for folder in ${fitfolders[@]}
+do
+	cd /scratch/adam/$folder
+	for file in $(ls *.xml)
+		do $rapidfit -f $file
+		/scratch/adam/BsphiKK/fits/output/mergeprojections.sh
+		/scratch/adam/BsphiKK/fits/output/compareresult.sh
+		mkdir -p FitResult_$(echo $file | sed 's/\.xml//g')
+		mv -v RapidFitOutput* FitResult_$(echo $file | sed 's/\.xml//g')
+	done
+done
