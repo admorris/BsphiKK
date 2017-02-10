f9473b8f2ef1304569bd7a59393ab2d373d7651d
Adam Morris <adam.morris@cern.ch>
2017-02-10 10:15:02 +0000

    factor out different parallel environments for PPE and ECDF

--- fits/buildRF.sh
+++ fits/buildRF.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+source RFjobconfig.sh
 currentdir=$(pwd)
 submission_script=tmpbuildjob.sh
 cat <<-EOF > ${submission_script}
 	#!/bin/bash
 	#$ -N buildRapidFit
 	#$ -cwd
-	#$ -pe mpi 4
+	$ParallelEnv
 	#$ -l h_vmem=1G
 	# Don't run if there are jobs in the queue
 	#$ -hold_jid j_*
 	# Hard runtime limit
-	#$ -l h_rt=00:05:00 
+	#$ -l h_rt=00:10:00
 	source $currentdir/RFjobconfig.sh
+	SetupEnvironment
 	cd \$RapidFitDir
 	make -j
 	EOF
