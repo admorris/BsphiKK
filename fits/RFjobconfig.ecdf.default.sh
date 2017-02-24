14b51b660252d07d409fae06849600d9376b5e8c
Adam Morris <adam.morris@cern.ch>
2017-02-24 17:50:16 +0000

    another stupid error

--- fits/RFjobconfig.ecdf.default.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# RapidFit location
-export RapidFitDir=~/RapidFit
-export ParallelEnv="#$ -pe sharedmem 4"
-# Load ROOT and TexLive
-export SetupEnvironment=". /etc/profile.d/modules.sh\nmodule load root\nmodule load igmm/apps/texlive"
-
