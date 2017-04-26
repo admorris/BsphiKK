eddd864d04943ef38e28cd06ef9bcf7f9f00ab39
Adam Morris <adam.morris@cern.ch>
2017-04-26 17:17:34 +0100

    handle 3 projectsion

---
+++ fits/output/calculateBF.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function getfrac()
+{
+	echo $(grep "$1" $2) | sed "s/$1 //"
+}
+Nphi=$(getfrac "1phi1020(1,BW):" $1)
+BFphitoKK=""
+Nftwop=$(getfrac "1ftwop1525LHCb(2,BW):" $1)
+BFftwoptoKK=""
+if [[ "$Nphi" == "" || "$Nftwop" == "" ]]
+then
+	exit 0
+fi
+echo "ϕ frac: $Nphi"
+echo "f2´ frac: $Nftwop"
+echo "($Nftwop/$Nphi)*()" | bc -l
+
