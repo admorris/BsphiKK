93f268cfe8960b63dea1895d04ae440bd23a54e1
Adam Morris <adam.morris@cern.ch>
2017-05-17 14:14:12 +0100

    don't bother with the second fit

--- scripts/fitLb.sh
+++ scripts/fitLb.sh
@@ -2,27 +2,28 @@
 source cuts.sh
 cd ../ntuples
 table=../scripts/tables/MassFits.csv
-cutapplier BsphiKK_data_mvaVars.root "DecayTree" "abs(phiKpM-${Lbmass})<${Lbwindow}&&BCON_KK_M<1800" LbphiKp_data_mvaVars.root
+Lbfitwindow=60
+cutapplier BsphiKK_data_mvaVars.root "DecayTree" "abs(phiKpM-${Lbmass})<${Lbfitwindow}&&BCON_KK_M<1800" LbphiKp_data_mvaVars.root
 ../bin/FitLb --file LbphiKp_data_mvaVars.root \
              --Sfile LbphiKp_MC_mvaVars.root \
              --model "Crystal Ball + 2 Gaussians" \
-             --upper $(echo "${Lbmass}+${Lbwindow}" | bc -l) \
-             --lower $(echo "${Lbmass}-${Lbwindow}" | bc -l) \
+             --upper $(echo "${Lbmass}+${Lbfitwindow}" | bc -l) \
+             --lower $(echo "${Lbmass}-${Lbfitwindow}" | bc -l) \
              --plot ../latex/figs/LbphiKp_fit_for_yield \
-             --bins $(echo "${Lbwindow}/2" | bc -l | sed 's/\..*//') \
+             --bins $(echo "${Lbfitwindow}/2" | bc -l | sed 's/\..*//') \
              --sweight \
              --save-results LbphiKpdatafit \
              --output-file ${table} \
              --pulls
 
-../bin/FitLb --file LbphiKp_data_mvaVars.root \
-             --Sfile LbphiKp_MC_mvaVars.root \
-             --model "Crystal Ball + 2 Gaussians" \
-             --upper $(echo "${Lbmass}+${Lbwindow}" | bc -l) \
-             --lower $(echo "${Lbmass}-${Lbwindow}" | bc -l) \
-             --cuts "${LbphiKpveto}" \
-             --plot ../latex/figs/LbphiKp_fit_for_yield_vetoes \
-             --bins $(echo "${Lbwindow}/2" | bc -l | sed 's/\..*//') \
-             --pulls
+#../bin/FitLb --file LbphiKp_data_mvaVars.root \
+#             --Sfile LbphiKp_MC_mvaVars.root \
+#             --model "Crystal Ball + 2 Gaussians" \
+#             --upper $(echo "${Lbmass}+${Lbwindow}" | bc -l) \
+#             --lower $(echo "${Lbmass}-${Lbwindow}" | bc -l) \
+#             --cuts "${LbphiKpveto}" \
+#             --plot ../latex/figs/LbphiKp_fit_for_yield_vetoes \
+#             --bins $(echo "${Lbwindow}/2" | bc -l | sed 's/\..*//') \
+#             --pulls
 
 
