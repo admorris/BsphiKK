2b152f10da167c5b2bde14841f3f901168cee970
Adam Morris <adam.morris@cern.ch>
2017-05-25 13:50:13 +0100

    less magic

--- src/app/sigfraccalc.cpp
+++ src/app/sigfraccalc.cpp
@@ -5,6 +5,9 @@
 #include <fstream>
 #include "datum.h"
 #define expected_num_args 6
+#define mKK_lower_lim 1020
+#define mKK_upper_lim 2000
+#define minfrac 90
 
 int main(int argc, char* argv[])
 {
@@ -15,7 +18,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 	int m = std::atoi(argv[1]);
-	if(m < 1020 || m > 2000)
+	if(m < mKK_lower_lim || m > mKK_upper_lim)
 			std::cerr << "Warning: mKK limit is not between 1.02 and 2 GeV. This seems unlikely." << std::endl;
 	datum S  = {std::atof(argv[2]), std::atof(argv[3])};
 	datum B  = {std::atof(argv[4]), std::atof(argv[5])};
@@ -25,15 +28,15 @@ int main(int argc, char* argv[])
 			std::cerr << "One argument is <= 0. Will not continue." << std::endl;
 			return 1;
 		}
-	datum f  = {S.val()/(S.val()+B.val()), S.val() * B.err()/((S.val()+B.val())*(S.val()+B.val()))};
+	datum f = {S.val()/(S.val()+B.val()), S.val() * B.err()/((S.val()+B.val())*(S.val()+B.val()))};
 	std::cout << " With m(KK) < " << std::setprecision(4) << m << " GeV, the signal fraction is " << f << std::endl;
 	if(f.val() <= 0 || std::isnan(f.val()))
 	{
 		std::cerr << "Impossible fraction. Will not continue." << std::endl;
 		return 1;
 	}
-	else if (f.val() < 0.9)
-		std::cerr << "Warning: fraction is below 90%. This seems unlikely." << std::endl;
+	else if (f.val() < minfrac/100.)
+		std::cerr << "Warning: fraction is below " << minfrac << "%. This seems unlikely." << std::endl;
 	// Write the XML file
 	std::ofstream XML("signal_fraction_" + std::to_string(m) + "_fixed.xml", std::ios::out);
 	XML << "# S: " << S << "\n"
