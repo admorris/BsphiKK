7712c0d49ae046a334298b7129a53fcab1dfa376
Adam Morris <adam.morris@cern.ch>
2017-05-30 14:28:57 +0100

    general improvements

--- src/app/timeacc.cpp
+++ src/app/timeacc.cpp
@@ -30,36 +30,47 @@ int main (int argc, char const* argv[])
 	int nbins = 50;
 	for(const auto& function: functions)
 	{
-		TH1D* hist = MakeBranchPlot(file.c_str(),branch.c_str(),"",weight.c_str(),tlow,thi,nbins);
+		// Get the data
+		auto hist = std::unique_ptr<TH1D>(MakeBranchPlot(file.c_str(),branch.c_str(),"",weight.c_str(),tlow,thi,nbins));
+		// Test the formula
 		std::string formula = "x>[2]?[0]*("+function.formula+"):0";
 		TFormula test;
 		if(test.Compile(formula.c_str()) != 0)
 			continue;
+		// Construct the fit function
 		TF1 fitfunc("fitfunc",formula.c_str(),tlow,thi);
 		fitfunc.SetParNames("A","b","t_0");
 		fitfunc.SetParameters(1,2,0.1);
-		fitfunc.SetLineStyle(2);
-		fitfunc.SetLineColor(2);
+		// Perform the fit
 		hist->Fit(&fitfunc);
+		// Scale everything so it asymptotes to 1
 		double norm = fitfunc.GetParameter(0);
 		fitfunc.SetParameter(0,1);
-		hist = MakeBranchPlot(file.c_str(),branch.c_str(),"",weight.c_str(),tlow,thi,nbins);
+		hist = std::unique_ptr<TH1D>(MakeBranchPlot(file.c_str(),branch.c_str(),"",weight.c_str(),tlow,thi,nbins));
 		hist->Scale(1/norm);
-		plotmaker<TH1> plot(hist);
+		// Plot the data
+		plotmaker<TH1> plot(hist.get());
 		plot.SetTitle("Decay time","ps");
-		TCanvas* canv = plot.Draw("E1");
+		TCanvas* canv = plot.Draw("E1"); // The plotmaker class retains ownership of this TCanvas and will delete it on destruction
 		hist->GetYaxis()->SetTitle("Time acceptance (arbitrary units)");
-		TLatex blurb;
-		blurb.DrawLatex(2,0.6,("#font[132]{#it{#varepsilon}(#it{t}) = "+function.latex+"}").c_str());
+		// Overlay the fit function
+		fitfunc.SetLineStyle(2);
+		fitfunc.SetLineColor(2);
 		fitfunc.Draw("Lsame");
+		// Figure out where acceptance is 50%
 		double thalf = fitfunc.GetParameter(2);
 		for(; fitfunc.Eval(thalf) < 0.5; thalf+=1e-7) {}
 		std::cout << "ε(" << thalf << " ps) = " << fitfunc.Eval(thalf) << std::endl;
 		std::cout << "ε(" << thalf/hbar << " eV^−1) = " << fitfunc.Eval(thalf) << std::endl;
+		// Draw a vertical line where acceptance is 50%
 		TLine line(thalf, 0, thalf, 0.5);
 		line.Draw("same");
 		line.SetLineStyle(2);
+		// Draw some information
+		TLatex blurb;
+		blurb.DrawLatex(2,0.6,("#font[132]{#it{#varepsilon}(#it{t}) = "+function.latex+"}").c_str());
 		blurb.DrawLatex(2,0.3,("#font[132]{#it{#varepsilon}(#it{t}) = 0.5 at #it{t} = " + std::to_string(thalf) + " ps}").c_str());
+		// Save the plot
 		canv->SaveAs(("timeacc_"+function.name+".pdf").c_str());
 	}
 	return 0;
