e26cee2472c4657b43fd98486e6a6ca78ed92e82
Adam Morris <adam.morris@cern.ch>
2017-06-20 17:35:59 +0100

    improve

--- fits/scripts/assessfitresult.sh
+++ fits/scripts/assessfitresult.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function getnumber() # return a number from something of the form "<string>:<whitespace><float>"
 {
-	returnval=$(grep "$1\s*:" $2 | sed "s/.*${1}\s*:\s*//" | sed -r 's/([0-9\.]*).*/\1/' | tail -1)
-	if [[ -z $returnval ]]
+	returnval=$(grep "$1\s*:" $2 | sed "s/.*${1}\s*:\s*//" | sed -r 's/([0-9\.e\-\+]*).*/\1/' | tail -1)
+	if [[ -z $returnval || $returnval == *"e"* ]]
 	then
 		returnval="0"
 	fi
@@ -10,7 +10,7 @@ function getnumber() # return a number from something of the form "<string>:<whi
 }
 function getnumbers()
 {
-	returnval=$(grep "$1\s*:" $2 | sed "s/.*${1}\s*:\s*//" | sed -r 's/([0-9\.]*).*/\1/' | tail -${3} | tr '\n' ' ')
+	returnval=$(grep "$1\s*:" $2 | sed "s/.*${1}\s*:\s*//" | sed -r 's/([0-9\.e\-\+]*).*/\1/' | tail -${3} | tr '\n' ' ')
 	declare format
 	for i in $(seq ${3})
 	do
@@ -22,24 +22,30 @@ function paramsatlimit()
 {
 	echo $(grep "Caution: Parameter" $1 | sed -r 's/Caution: Parameter (\S*) .*$/\1/g' | tr '\n' ' ')
 }
+function parseparticlenames()
+{
+	echo $1 | sed 's/_/, /g' | sed 's/phi/phi /g' | sed 's/fzero/f0 /g' | sed "s/ftwop/f2' /g" | sed 's/LHCb//g' | sed -r 's/ ([0-9]+)(,?)/(\1)\2/g'
+}
 cd $1
 cwd=$(pwd)
-printf '%-70s & %-6s & %-6s & %-6s & %-6s & %23s & %-13s & %-20s & %-18s \\\\ %% Parameters at limit\n' "folder" "status" "NLL" "AIC" "BIC" "Projection Chi^2/ndof" "\$\\phi\\phi\$ \\%" "\$\\phi f_2^\\prime\$ \\%" "\$f_2^\\prime\$ \$f_L\$"
+printf '%-70s | %-6s | %-6s | %-6s | %-6s | %24s | %-5s | %-5s | %-5s | %-7s | %-4s | %-6s | %% Parameters at limit\n' "folder" "status" "NLL" "AIC" "BIC" "Projection χ^2/ndof" "NR f" "f0 f" "phi f" "phi F_0" "f2' f" "f2' F_0"
 for folder in $(ls | grep FitResult)
 do
 	cd $cwd/$folder
-	for file in $(ls | grep "RapidFitOutput-.*\.log" | tail -1)
+	for file in $(ls | grep "RapidFitOutput-.*\.log")
 	do
 		chisq=$(getnumbers "Chi\^2\/ndof " $file 4 | tr '\n' ' ')
 		nll=$(getnumber "NLL" $file)
 		aic=$(getnumber "AIC" $file)
 		bic=$(getnumber "BIC" $file)
+		NRfrac=$(getnumber "1nonres" $file)
+		fzfrac=$(getnumber "1fzero980" $file)
 		phifrac=$(getnumber   "1phi1020" $file)
+		phifL=$(getnumber   "phi1020\_Azerosq" $file)
 		ftwopfrac=$(getnumber "1ftwop1525LHCb" $file)
 		ftwopfL=$(getnumber   "ftwop1525LHCb\_Azerosq" $file)
 		status=$(getnumber "Status" $file | tail -1)
-		#echo "${folder/FitResult_/} & ${status} & ${nll} & ${aic} & ${bic} & ${chisq} & ${phifrac} & ${ftwopfrac} & ${ftwopfL} \\\\"
-		printf '%-70s & %6d & %6.1f & %6.1f & %6.1f & %5.3f %5.3f %5.3f %5.3f & %13.3f & %20.3f & %18.3f \\\\ %% %-20s \n' ${folder/FitResult_/} ${status} ${nll} ${aic} ${bic} ${chisq} ${phifrac} ${ftwopfrac} ${ftwopfL} "$(paramsatlimit $file)"
+		printf '%-70s | %6d | %6.1f | %6.1f | %6.1f | %5.3f %5.3f %5.3f %5.3f | %5.3f | %5.3f | %5.3f | %7.3f | %5.3f | %7.3f | %-20s \n' "$(parseparticlenames ${folder/FitResult_/})" ${status} ${nll} ${aic} ${bic} ${chisq} ${NRfrac} ${fzfrac} ${phifrac} ${phifL} ${ftwopfrac} ${ftwopfL} "$(paramsatlimit $file)"
 	done
 done
 
