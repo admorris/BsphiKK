16f48e88f62e4d078dc3ea15217dc81ff73aba6f
Adam Morris <adam.morris@cern.ch>
2017-07-06 15:34:32 +0100

    toy jobs for stat err and acc syst

--- fits/toyjobs_acceptance.sh
+++ fits/toyjobs_acceptance.sh
@@ -2,7 +2,6 @@
 # Usage: toyjobs.sh <folder> [<timestamp>]
 source RFjobconfig.sh
 currentdir=$(pwd)
-nrepeats=16
 if [ "$1" == "" ]
 then
 	echo "Please provide a folder containing the RapidFit outputXMLFile"
@@ -12,7 +11,7 @@ else
 fi
 if [ "$2" == "" ]
 then
-	recentxml=$(find $ -type f -name "outputXMLFile*" | sort | tail -1)
+	recentxml=$(find $folder -type f -name "outputXMLFile*" | sort | tail -1)
 	timestamp=$(echo ${recentxml} | sed -r 's/.*outputXMLFile(.*)\.xml/\1/')
 else
 	recentxml="outputXMLFile${2}.xml"
@@ -21,16 +20,16 @@ fi
 for i in `seq 0 15`
 do
 	cd ${currentdir}/${folder}
-	workingfolder="toyjob_${timestamp}_${i}"
+	workingfolder="acceptancejob_${timestamp}_${i}"
 	mkdir -p ${workingfolder}
 	cd ${workingfolder}
 	submission_script="submit.sh"
 	cat <<-EOF > ${submission_script}
 	#!/bin/bash
-	#$ -N "j_toy_${timestamp}_${i}"
+	#$ -N "j_toy_acc_${timestamp}_${i}"
 	#$ -l h_rt=24:00:00
 	#$ -l h_vmem=4G
-		#$ -l h='!morar2.ph.ed.ac.uk'
+	#$ -l h='!morar2.ph.ed.ac.uk'
 	$ParallelEnv
 	#$ -cwd
 	#$ -hold_jid buildRapidFit
@@ -44,8 +43,16 @@ do
 	echo -e "$SetupEnvironment" >> ${submission_script}
 	cat <<-EOF >> ${submission_script}
 	export PATH=\$PATH:\$RapidFitDir/bin
-	# Perform the fit
-	fitting ${nThreadsFlag} -f ../${recentxml} --calculateFitFractions -repeats ${nrepeats} --useUUID | tee RapidFitOutput-\$(date +"%Y%m%d_%H%M%S").log
+	for acc in \`seq 0 7\`
+	do
+		index=\$(($i*8+\$acc))
+		newXMLfile=\$(echo "${recentxml}" | sed "s/\./_\${index}/")
+		sed "s/AcceptanceEntry:0/AcceptanceEntry:\${index}/" ${recentxml} > \$newXMLfile
+		# Perform the fit
+		logfile=RapidFitOutput-\$(date +"%Y%m%d_%H%M%S")_acceptance_\${index}.log
+		fitting ${nThreadsFlag} -f ../${file} --OverrideXML /RapidFit/ToFit/DataSet/NumberEvents 100000 --generateToyXML --MultiDimChi2 --ForceContinue $3 2>&1| tee \${logfile}
+		$currentdir/scripts/calculatefitfractions.sh 2>&1| tee -a \${logfile}
+	done
 	EOF
 	# Submit the jobs
 	qsub ${submission_script}
