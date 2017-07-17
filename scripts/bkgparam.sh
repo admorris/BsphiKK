d5362feb97d1eda2edd9edbcc8fe798b075a88df
Adam Morris <adam.morris@cern.ch>
2017-07-17 18:56:41 +0100

    phi K* histogram

--- scripts/bkgparam.sh
+++ scripts/bkgparam.sh
@@ -25,12 +25,12 @@ ${bindir}/PlotAngAcc sampled_combinatorial_nomva.root
 rename acceptance background_nomva *.pdf
 }
 # phi K* peaking background
-#fitting -f phikstar_background.xml --calculateBackgroundCoefficients ## Angular part only
-#xrdcp --verbose --force LegendreMoments_1.root ${nTuples_dir}/LegendreMoments_phikstar.root
-#rm -v LegendreMoments_1.root
-#mv -v sampled_LegendreMomentShape_1.root sampled_phikstar.root
-#${bindir}/PlotAngAcc sampled_phikstar.root
-#rename acceptance phikstar_background *.pdf
+fitting -f phikstar_background.xml --calculateBackgroundCoefficients ## Angular part only
+xrdcp --verbose --force LegendreMoments_1.root ${nTuples_dir}/LegendreMoments_phikstar.root
+rm -v LegendreMoments_1.root
+mv -v sampled_LegendreMomentShape_1.root sampled_phikstar.root
+${bindir}/PlotAngAcc sampled_phikstar.root
+rename acceptance phikstar_background *.pdf
 mv -v *background_*pdf ${figsdir}/
 # Histogram for the mass-dependent part
 ${bindir}/PlotBranch  -F ${local_nTuples_dir}BsphiKK_data_mvacut.root \
@@ -61,16 +61,16 @@ mv -v $outputfilename.pdf ${figsdir}/background_nomva_mKK_proj.pdf
 xrdcp --verbose --force $outputfilename.root ${nTuples_dir}/background_nomva_mKKhist.root
 rm -v $outputfilename.root
 # Histogram for the mass-dependent part
-#${bindir}/PlotBranch  -F ${local_nTuples_dir}BdphiKst_MC_mvacut.root \
-#                     -B BCON_KK_M_GeV \
-#                     -T "#it{m}(#it{K^{#plus}K^{#minus}})" \
-#                     -U "GeV/#it{c}^{2}" \
-#                     -l 0.980 \
-#                     -u 1.800 \
-#                     -b 41 \
-#                     -O $outputfilename \
-#                     --root
-#mv -v $outputfilename.pdf ${figsdir}/phikstar_background_mKK_proj.pdf
-#xrdcp --verbose --force $outputfilename.root ${nTuples_dir}/phikstar_background_mKKhist.root
-#rm -v $outputfilename.root
+${bindir}/PlotBranch  -F ${local_nTuples_dir}BdphiKst_MC_mvacut.root \
+                     -B BCON_KK_M_GeV \
+                     -T "#it{m}(#it{K^{#plus}K^{#minus}})" \
+                     -U "GeV/#it{c}^{2}" \
+                     -l 0.980 \
+                     -u 1.800 \
+                     -b 41 \
+                     -O $outputfilename \
+                     --root
+mv -v $outputfilename.pdf ${figsdir}/phikstar_background_mKK_proj.pdf
+xrdcp --verbose --force $outputfilename.root ${nTuples_dir}/phikstar_background_mKKhist.root
+rm -v $outputfilename.root
 
