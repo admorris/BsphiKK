ba14e5ef3a8544848c274d3aeb1ba4737ea13469
Adam Morris <adam.morris@cern.ch>
2017-07-25 13:55:31 +0100

    oops

--- fits/scripts/generatedatafits_1680_1710.sh
+++ fits/scripts/generatedatafits_1680_1710.sh
@@ -6,7 +6,7 @@ then
 	exit 1
 fi
 bindir="${CWD}/../bin"
-srcdir="${CWD}/src/datafits/1680_1500_extra"
+srcdir="${CWD}/src/datafits/1680_1710_extra"
 best=(nonres fzero980 phi1020 ftwop1525 phi1680 fzero1710)
 mkdir -p ${srcdir}
 for extra in "fzero1370LHCb" "ftwo1640" "fzero1500LHCb" "ftwo1750"
