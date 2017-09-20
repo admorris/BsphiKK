e820d6f7bd4c6e855adcc67322083633f93d9d3d
Adam Morris <adam.morris@cern.ch>
2017-09-20 18:47:32 +0100

    variations on the angular acceptance

--- scripts/angacc.sh
+++ scripts/angacc.sh
@@ -2,7 +2,14 @@
 source eos.sh
 cd ../fits/results
 make -C ../modules -j
-for trigger in TIS TOS
+trigger=Gen
+fitting -f acceptance$trigger.xml --calculateAcceptanceCoefficients
+mv -v LegendreMoments_1.root LegendreMoments_Acceptance_$trigger.root
+mv -v sampled_LegendreMomentShape_1.root sampled_LegendreMomentShape_$trigger.root
+xrdcp --verbose --force LegendreMoments_Acceptance_*.root ${nTuples_dir}
+rename LegendreMomentShape acceptance sampled_LegendreMomentShape_*.root
+
+for trigger in TIS TOS TOS_PIDcalib
 do
   fitting -f acceptance$trigger.xml --calculateAcceptanceCoefficients
   mv -v LegendreMoments_1.root LegendreMoments_Acceptance_not$trigger.root
@@ -11,10 +18,10 @@ do
   mv -v sampled_LegendreMomentShape_2.root sampled_LegendreMomentShape_$trigger.root
   xrdcp --verbose --force LegendreMoments_Acceptance_*.root ${nTuples_dir}
   rename LegendreMomentShape acceptance sampled_LegendreMomentShape_*.root
-  for file in $(ls sampled_acceptance_*.root)
-  do
-    ../../bin/PlotAngAcc $file $(echo $file | sed 's/sampled_acceptance_//g' | sed 's/\.root//g')
-  done
+done
+for file in $(ls sampled_acceptance_*.root)
+do
+  ../../bin/PlotAngAcc $file $(echo $file | sed 's/sampled_acceptance_//g' | sed 's/\.root//g')
 done
 mv -v acceptance_*pdf ../../latex/figs/
 
