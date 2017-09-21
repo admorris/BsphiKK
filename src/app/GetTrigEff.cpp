fa833dd312c94f7eb55994124cd7d23cc7308cb6
Adam Morris <adam.morris@cern.ch>
2017-09-22 00:01:10 +0100

    justify removed lines

--- src/app/GetTrigEff.cpp
+++ src/app/GetTrigEff.cpp
@@ -13,7 +13,7 @@ void GetTrigEff(string filename, bool save, string DBfilename)
 {
   string L0[]={"B_s0_L0HadronDecision_TOS", "B_s0_L0Global_TIS"};
   string Hlt1="B_s0_Hlt1TrackAllL0Decision_TOS";
-  string Hlt2[]={"B_s0_Hlt2Topo3BodyBBDTDecision_TOS", "B_s0_Hlt2Topo4BodyBBDTDecision_TOS"};
+  string Hlt2[]={"B_s0_Hlt2Topo3BodyBBDTDecision_TOS", "B_s0_Hlt2Topo4BodyBBDTDecision_TOS", "B_s0_Hlt2Topo2BodyBBDTDecision_TOS"};
   TTree* intree = GetTree(filename);
   new TCanvas;
   CutResult_t L0eff[]   = 
@@ -23,13 +23,16 @@ void GetTrigEff(string filename, bool save, string DBfilename)
   };
   CutResult_t totL0eff  = CutEff(intree,"",L0[0]+"||"+L0[1]);
   CutResult_t Hlt1eff   = CutEff(intree,L0[0]+"||"+L0[1],Hlt1);
-  CutResult_t Hlt2eff[] = 
+  CutResult_t Hlt2eff[] =
   {
     CutEff(intree,"("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",Hlt2[0])
   , CutEff(intree,"("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",Hlt2[1])
+  , CutEff(intree,"("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",Hlt2[2])
   };
   CutResult_t totHlt2eff = CutEff(intree,   "("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",   Hlt2[0]+"||"+Hlt2[1]    );
   CutResult_t toteff     = CutEff(intree,"","("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")&&("+Hlt2[0]+"||"+Hlt2[1]+")");
+  CutResult_t totHlt2eff_extra = CutEff(intree,   "("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",   Hlt2[0]+"||"+Hlt2[1]+"||"+Hlt2[2]    );
+  CutResult_t toteff_extra     = CutEff(intree,"","("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")&&("+Hlt2[0]+"||"+Hlt2[1]+"||"+Hlt2[2]+")");
   cout << "Line & Efficiency \\\\" << endl
   << L0[0]   << " & " << L0eff[0].GetEff()*100   << "\\% \\\\" << endl
   << L0[1]   << " & " << L0eff[1].GetEff()*100   << "\\% \\\\" << endl
@@ -39,6 +42,9 @@ void GetTrigEff(string filename, bool save, string DBfilename)
   << Hlt2[1] << " & " << Hlt2eff[1].GetEff()*100 << "\\% \\\\" << endl
   << "Hlt2"  << " & " << totHlt2eff.GetEff()*100 << "\\% \\\\" << endl
   << "Total" << " & " << toteff.GetEff()*100     << "\\% \\\\" << endl
+  << "\\hline" << endl
+  << "Hlt2 w T2B"  << " & " << totHlt2eff_extra.GetEff()*100 << "\\% \\\\" << endl
+  << "Total w T2B" << " & " << toteff_extra.GetEff()*100     << "\\% \\\\" << endl
   ;
   if(save)
   {
