18cc52370771912e515ab219ce891e77e4fbabc2
Adam Morris <adam.morris@cern.ch>
2017-09-22 00:02:49 +0100

    justify removed lines

--- src/app/PlotTrigEff.cpp
+++ src/app/PlotTrigEff.cpp
@@ -6,7 +6,7 @@ void PlotTrigEff(std::string filename,std::string plotname)
 {
   std::string L0[]={"B_s0_L0HadronDecision_TOS", "B_s0_L0Global_TIS"};
   std::string Hlt1="B_s0_Hlt1TrackAllL0Decision_TOS";
-  std::string Hlt2[]={"B_s0_Hlt2Topo3BodyBBDTDecision_TOS", "B_s0_Hlt2Topo4BodyBBDTDecision_TOS"};
+  std::string Hlt2[]={"B_s0_Hlt2Topo3BodyBBDTDecision_TOS", "B_s0_Hlt2Topo4BodyBBDTDecision_TOS", "B_s0_Hlt2Topo2BodyBBDTDecision_TOS", "B_s0_Hlt2IncPhiDecision_TOS"};
   const unsigned int nbins = 20;
   double mKKlo = 990, mKKhi = 1800, binw = (mKKhi-mKKlo)/nbins;
   double mKK[nbins], mKKE[nbins];
@@ -19,6 +19,8 @@ void PlotTrigEff(std::string filename,std::string plotname)
   Plots.push_back(TrigEffPlot("Hlt1Total",nbins,"&& ("+L0[0]+"||"+L0[1]+")",   Hlt1                                ));
   Plots.push_back(TrigEffPlot("Topo3Body",nbins,"&& ("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",   Hlt2[0]                 ));
   Plots.push_back(TrigEffPlot("Topo4Body",nbins,"&& ("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",                Hlt2[1]    ));
+  Plots.push_back(TrigEffPlot("Topo2Body",nbins,"&& ("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",   Hlt2[2]                 ));
+  Plots.push_back(TrigEffPlot("IncPhiDec",nbins,"&& ("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",                Hlt2[3]    ));
   Plots.push_back(TrigEffPlot("Hlt2Total",nbins,"&& ("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")",   Hlt2[0]+"||"+Hlt2[1]    ));
   Plots.push_back(TrigEffPlot("TrigTotal",nbins,"","("+L0[0]+"||"+L0[1]+")&&("+Hlt1+")&&("+Hlt2[0]+"||"+Hlt2[1]+")"));
   for(auto Plot : Plots)
